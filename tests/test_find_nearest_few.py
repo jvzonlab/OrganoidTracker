8cf6218ccdbf90a29df0c84c165ed63c8a71f19e
Rutger Kok <rutger.kok@live.com>
2018-02-16 13:56:45 +0100

    Update tests for code changes

--- tests/test_find_nearest_few.py
+++ tests/test_find_nearest_few.py
@@ -1,21 +1,24 @@
 import unittest
-from imaging.io import Frame, Particle
+from imaging import Frame, Particle
 from nearest_neighbor_linking.find_nearest_few import find_nearest_particles
 
 
 class TestFindNearestFew(unittest.TestCase):
 
     def test_find_two(self):
-        frame = Frame(2, [Particle(10,20,0), Particle(11,20,0), Particle(100,20,0)])
+        frame = Frame(2)
+        frame.add_particles([Particle(10,20,0), Particle(11,20,0), Particle(100,20,0)])
         found = find_nearest_particles(frame, Particle(40,20,0), 1.1)
         self.assertEqual(2, len(found), "Expected to find two particles that are close to each other")
 
     def test_find_one(self):
-        frame = Frame(2, [Particle(10, 20, 0), Particle(11, 20, 0), Particle(100, 20, 0)])
+        frame = Frame(2)
+        frame.add_particles([Particle(10, 20, 0), Particle(11, 20, 0), Particle(100, 20, 0)])
         found = find_nearest_particles(frame, Particle(80, 20, 0), 1.1)
         self.assertEqual(1, len(found), "Expected to find one particle that is close enough")
 
     def test_zero_tolerance(self):
-        frame = Frame(2, [Particle(10,20,0), Particle(11,20,0), Particle(100,20,0)])
+        frame = Frame(2)
+        frame.add_particles([Particle(10,20,0), Particle(11,20,0), Particle(100,20,0)])
         found = find_nearest_particles(frame, Particle(40,20,0), 1)
         self.assertEqual(1, len(found), "Tolerance is set to 1.0, so only one particle may be found")
