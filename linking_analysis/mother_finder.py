3a6cbcea319e1a9b770660f4ea18c73e279a462c
Rutger Kok <rutger.kok@live.com>
2018-03-07 13:46:32 +0100

    Fix daughters that are swapped around causing a problem

--- linking_analysis/mother_finder.py
+++ linking_analysis/mother_finder.py
@@ -24,13 +24,11 @@ def find_mothers(graph: Graph) -> Set[Particle]:
 class Family:
     """A mother cell with two daughter cells."""
     mother: Particle
-    daughter1: Particle
-    daughter2: Particle
+    daughters: Set[Particle]
 
     def __init__(self, mother: Particle, daughter1: Particle, daughter2: Particle):
         self.mother = mother
-        self.daughter1 = daughter1
-        self.daughter2 = daughter2
+        self.daughters = {daughter1, daughter2}
 
     @staticmethod
     def _pos_str(particle: Particle) -> str:
@@ -38,19 +36,21 @@ class Family:
 
     def __str__(self):
         return self._pos_str(self.mother) + " " + str(self.mother.time_point_number()) + "---> " \
-               + self._pos_str(self.daughter1) + " and " + self._pos_str(self.daughter2)
+               + " and ".join([self._pos_str(daughter) for daughter in self.daughters])
 
     def __repr__(self):
-        return "Family(" + repr(self.mother) + ", " + repr(self.daughter1) + ", " + repr(self.daughter2) + ")"
+        return "Family(" + repr(self.mother) + ", " +  ", ".join([repr(daughter) for daughter in self.daughters]) + ")"
 
     def __hash__(self):
-        return hash(self.mother) ^ hash(self.daughter1) ^ hash(self.daughter2)
+        hash_code = hash(self.mother)
+        for daughter in self.daughters:
+            hash_code += hash(daughter)
+        return hash_code
 
     def __eq__(self, other):
         return isinstance(other, self.__class__) \
             and other.mother == self.mother \
-            and other.daughter1 == self.daughter1 \
-            and other.daughter2 == self.daughter2
+            and other.daughters == self.daughters
 
 
 def find_families(graph: Graph) -> Set[Family]:
