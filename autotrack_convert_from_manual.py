99891edf51a136ee8a581b2ee69af82d9df955aa
Rutger Kok <rutger.kok@live.com>
2018-03-19 13:57:24 +0100

    Reduce code duplication by introducing a small configuration library

--- autotrack_convert_from_manual.py
+++ autotrack_convert_from_manual.py
@@ -2,57 +2,25 @@
 
 # This script is used to convert data from Guizela's scripts (e.g. track_manually.py) to data in our standard JSON
 # format. Just launch it
-
+from config import ConfigFile
 from manual_tracking import positions_extractor, links_extractor
 from imaging import io
-from configparser import ConfigParser
 
 # CONFIGURATION
-config_file = "autotrack.ini"
-config_changed = False
-print("Hi! Configuration file is stored at " + config_file)
-config = ConfigParser()
-config.read(config_file)
-if "DEFAULT" not in config:
-    config["DEFAULT"] = {}
-if "convert_from_manual" not in config:
-    config["convert_from_manual"] = {}
-    config_changed = True
-our_section = config["convert_from_manual"]
-if "image_folder" not in our_section:
-    config["DEFAULT"]["image_folder"] = "."
-    config_changed = True
-if "min_time_point" not in our_section:
-    config["DEFAULT"]["min_time_point"] = str(1)
-    config_changed = True
-if "max_time_point" not in our_section:
-    config["DEFAULT"]["max_time_point"] = str(9999)
-    config_changed = True
-if "positions_file" not in our_section:
-    our_section["positions_file"] = "Automatic analysis/Positions/Manual.json"
-    config_changed = True
-if "links_file" not in our_section:
-    our_section["links_file"] = "Automatic analysis/Links/Manual.json"
-    config_changed = True
-if "input_tracks_folder" not in our_section:
-    our_section["input_tracks_folder"] = input("Please enter the name of the folder where the track_xxxxx.p files are stored:")
-    config_changed = True
-if config_changed:
-    with open(config_file, 'w') as config_writing:
-        config.write(config_writing)
-    if input("Configuration file was changed. Type start to run the script now.") != "start":
-        print("Typed something else, bye!")
-        exit(0)
-    print("Ok, let's start!")
+print("Hi! Configuration file is stored at " + ConfigFile.FILE_NAME)
+config = ConfigFile("convert_from_manual")
+_min_time_point = int(config.get_or_default("min_time_point", str(1), store_in_defaults=True))
+_max_time_point = int(config.get_or_default("max_time_point", str(9999), store_in_defaults=True))
+_positions_file = config.get_or_default("positions_file", "Automatic analysis/Positions/Manual.json")
+_links_file = config.get_or_default("links_file", "Automatic analysis/Links/Manual.json")
+_tracks_folder = config.get_or_prompt("input_tracks_folder", "Please enter the name of the folder where the track_xxxxx.p files are stored:")
+if config.save_if_changed():
+    print("Note: the configuration file was changed. Starting script...")
 # END OF CONFIGURATION
 
 
-positions_extractor.extract_positions(our_section["input_tracks_folder"], our_section["positions_file"],
-                                      min_time_point=int(our_section["min_time_point"]),
-                                      max_time_point=int(our_section["max_time_point"]))
-io.save_links_to_json(
-                      links_extractor.extract_from_tracks(our_section["input_tracks_folder"],
-                                                        min_time_point=int(our_section["min_time_point"]),
-                                                        max_time_point=int(our_section["max_time_point"])),
-                      our_section["links_file"])
+positions_extractor.extract_positions(_tracks_folder, _positions_file, min_time_point=_min_time_point,
+                                      max_time_point=_max_time_point)
+io.save_links_to_json(links_extractor.extract_from_tracks(_tracks_folder, min_time_point=_min_time_point,
+                                                          max_time_point=_max_time_point), _links_file)
 print("Done!")
---
+++ config/__init__.py
@@ -0,0 +1,69 @@
+from configparser import RawConfigParser
+
+
+class ConfigFile:
+    """Simple wrapper around ConfigParser"""
+
+    FILE_NAME = "autotrack.ini"
+
+    _config: RawConfigParser
+    _section_name: str
+    made_changes: bool = False  # Changes to True when the configuration file was changed.
+
+    def __init__(self, section_name: str):
+        """Creates a new configuration. Loads from disk using the standard file name. section_name is the section of
+        the config file that we're currently looking at. (Different scripts should store their settings in
+        different sections.)
+        """
+        self._section_name = section_name
+
+        self._config = RawConfigParser()
+        self._config.read(self.FILE_NAME)
+
+        # Create some sections
+        if "DEFAULTS" not in self._config:
+            self._config["DEFAULTS"] = {}
+            self.made_changes = True
+        if section_name not in self._config:
+            self._config[section_name] = {}
+            self.made_changes = True
+
+    def get_or_default(self, key: str, default_value: str, store_in_defaults: bool = False) -> str:
+        """Gets a string from the config file for the given key. If no such string exists, the default value is
+        stored in the config first. If store_in_defaults is True, then the default value is stored in the DEFAULTS
+        section, otherwise the default value is stored in the section given to __init__.
+        """
+        if key in self._config[self._section_name]:
+            return self._config[self._section_name][key]
+        if key in self._config["DEFAULTS"]:
+            return self._config["DEFAULTS"][key]
+
+        if store_in_defaults:
+            self._config["DEFAULTS"][key] = default_value
+        else:
+            self._config[self._section_name][key] = default_value
+        self.made_changes = True
+        return default_value
+
+    def get_or_prompt(self, key: str, question: str, store_in_defaults: bool = False):
+        """Gets a string from the config file for the given key. If no such string exists, the user is asked for a
+        default value, which is then stored in the configuration file. If store_in_defaults is True, then the default
+        value is stored in the DEFAULTS section, otherwise the default value is stored in the section given to __init__.
+        """
+        if key in self._config[self._section_name]:
+            return self._config[self._section_name][key]
+        if key in self._config["DEFAULTS"]:
+            return self._config["DEFAULTS"][key]
+
+        default_value = input(question + " ")
+        return self.get_or_default(key, default_value, store_in_defaults=store_in_defaults)
+
+    def save_if_changed(self) -> bool:
+        """Saves the configuration if it was changed. Returns True if the file was saved, False otherwise."""
+        if not self.made_changes:
+            return False
+        with open(self.FILE_NAME, 'w') as config_writing:
+            self._config.write(config_writing)
+        return True
+
+
