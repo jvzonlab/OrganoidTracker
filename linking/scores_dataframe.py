f1eaad44de1c056defdea0873a9c750f8e8fea62
Rutger Kok <rutger.kok@live.com>
2018-04-10 09:16:55 +0200

    Shorten header names in CSV output

--- linking/scores_dataframe.py
+++ linking/scores_dataframe.py
@@ -27,7 +27,19 @@ def create(experiment: Experiment, putative_families: List[Family], scoring_syst
         data[i, len(keys) - 2] = score.total()
         data[i, len(keys) - 1] = 1 if family in actual_families else 0
 
-    return pandas.DataFrame(data=data, columns=keys)
+    return pandas.DataFrame(data=data, columns=_table_names(keys))
+
+
+def _table_names(keys: List[str]) -> List[str]:
+    names = []
+    for key in keys:
+        if key.startswith("mother_"):
+            key = key.replace("mother_", "m_")
+        if key.startswith("daughters_"):
+            key = key.replace("daughters_", "d_")
+        names.append(key)
+    return names
+
 
 def _score(experiment: Experiment, family: Family, scoring_system: MotherScoringSystem) -> Score:
     daughters = list(family.daughters)
