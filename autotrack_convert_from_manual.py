c3836ff7975912d76986a1e7960b8ab9274d60a9
Rutger Kok <rutger.kok@live.com>
2018-06-12 17:12:02 +0200

    Restore the rudimentary shape detection in convert_from_manual
    This makes the program easier to use.

--- autotrack_convert_from_manual.py
+++ autotrack_convert_from_manual.py
@@ -1,33 +1,44 @@
 #!/usr/bin/env python3
 
-"""This script is used to convert data from Guizela's scripts (e.g. track_manually.py) to data in our standard JSON
-format."""
+# This script is used to convert data from Guizela's scripts (e.g. track_manually.py) to data in our standard JSON
+# format. Just launch it
 
 
 from config import ConfigFile
 from core import Experiment
-from imaging import io
+from imaging import io, tifffolder
 from manual_tracking import links_extractor
 
 # CONFIGURATION
+from particle_detection import ellipse_detection
+
 print("Hi! Configuration file is stored at " + ConfigFile.FILE_NAME)
 config = ConfigFile("convert_from_manual")
+_images_folder = config.get_or_default("images_folder", "./", store_in_defaults=True)
+_images_format = config.get_or_prompt("images_pattern", "What are the image file names? (Use %03d for a three digits "
+                                                        "representing the time)", store_in_defaults=True)
 _min_time_point = int(config.get_or_default("min_time_point", str(1), store_in_defaults=True))
 _max_time_point = int(config.get_or_default("max_time_point", str(9999), store_in_defaults=True))
 _positions_file = config.get_or_default("positions_file", "Automatic analysis/Positions/Manual.json")
 _links_file = config.get_or_default("links_file", "Automatic analysis/Links/Manual.json")
 _tracks_folder = config.get_or_prompt("input_tracks_folder", "Please enter the name of the folder where the track_xxxxx.p files are stored:")
+_shape_detection_radius = int(config.get_or_default("shape_detection_radius", str(16)))
 if config.save_if_changed():
     print("Note: the configuration file was changed. Starting script...")
 # END OF CONFIGURATION
 
 
 graph = links_extractor.extract_from_tracks(_tracks_folder, min_time_point=_min_time_point,
-                                            max_time_point=_max_time_point)
+                                                          max_time_point=_max_time_point)
 experiment = Experiment()
 for particle in graph.nodes():
     experiment.add_particle(particle)
 experiment.particle_links(graph)
+
+print("Performing rudimentary 2d shape detection...")
+tifffolder.load_images_from_folder(experiment, _images_folder, _images_format,
+                                   min_time_point=_min_time_point, max_time_point=_max_time_point)
+ellipse_detection.detect_for_all(experiment, _shape_detection_radius)
 
 io.save_positions_and_shapes_to_json(experiment, _positions_file)
 io.save_links_to_json(graph, _links_file)
