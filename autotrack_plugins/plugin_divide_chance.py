0a2dfd5a51175f3a321eb11d3a9be770d79f088a
Rutger Kok <rutger.kok@live.com>
2018-10-19 15:31:35 +0200

    Improve cell fate plugin to show fractions

--- autotrack_plugins/plugin_divide_chance.py
+++ autotrack_plugins/plugin_divide_chance.py
@@ -1,6 +1,7 @@
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, List
 
 from matplotlib.figure import Figure
+from matplotlib.ticker import FuncFormatter
 from networkx import Graph
 
 from autotrack.core import UserError
@@ -33,39 +34,36 @@ def _show_chance_of_division(experiment: Experiment):
 
 
 def _draw_histogram(figure: Figure, links: Graph, max_time_point_number: int):
-    dividing_cells, nondividing_cells, unknown_fate_cells = _classify_cell_divisions(links, max_time_point_number)
-    colors = ["orange", "blue", "lightgray"]
-    labels = ["Dividing", "Non-dividing", "Unknown"]
+    time_points_per_bin = 10
+    bins = _classify_cell_divisions(links, time_points_per_bin, max_time_point_number)
+    if len(bins) == 0:
+        raise UserError("No cell cycles found",
+                        "The linking data contains no full cell cycles, so we cannot plot anything.")
+
+    dividing_cell_fractions = [bin.dividing_fraction() for bin in bins]
+    nondividing_cell_fractions = [bin.nondividing_fraction() for bin in bins]
+    unknown_fate_cell_fractions = [bin.unknown_fraction() for bin in bins]
+    dividing_plus_unknown_cell_fractions = [bin.dividing_fraction() + bin.unknown_fraction() for bin in bins]
+    time_point_numbers = [bin.min_time_point_number for bin in bins]
 
     axes = figure.gca()
-    axes.hist([dividing_cells, nondividing_cells, unknown_fate_cells], bins=10, histtype="barstacked", color=colors,
-              label=labels)
+    axes.bar(time_point_numbers, dividing_cell_fractions,
+             align="edge", width=time_points_per_bin, label="Dividing", color="orange")
+    axes.bar(time_point_numbers, unknown_fate_cell_fractions, bottom=dividing_cell_fractions,
+             align="edge", width=time_points_per_bin, label="Unknown", color="lightgray")
+    axes.bar(time_point_numbers, nondividing_cell_fractions, bottom=dividing_plus_unknown_cell_fractions,
+             align="edge", width=time_points_per_bin, label="Nondividing", color="blue")
+    for some_bin in bins:
+        axes.text(some_bin.min_time_point_number + time_points_per_bin/2, 0.05,
+                  f"{some_bin.total_number_of_cells()} cells", horizontalalignment='center', fontsize=8)
     axes.set_xlabel("Duration of previous cell cycle (time points)")
-    axes.set_ylabel("Occurrences")
-    axes.set_title("Fate of cells dividing")
+    axes.set_ylabel("Cell fate (fractions)")
+    axes.set_title("Fate of cells after a division")
     axes.legend()
 
-
-def _classify_cell_divisions(links: Graph, max_time_point_number: int):
-    """Classifies each cell division in the data: is it the last cell division in a lineage, or will there be more after
-    this? Returns three lists, each containing numbers representing the time duration of the previous generation."""
-    dividing_cells = list()
-    nondividing_cells = list()
-    unknown_fate_cells = list()
-
-    for cell_division in mother_finder.find_families(links):
-        previous_cell_cycle_length = cell_cycle.get_age(links, cell_division.mother)
-        if previous_cell_cycle_length is None:
-            continue  # Cannot plot without knowing the length of the previous cell cycle
-        for daughter in cell_division.daughters:
-            will_divide = _will_divide(links, daughter, max_time_point_number)
-            if will_divide is None:
-                unknown_fate_cells.append(previous_cell_cycle_length)
-            elif will_divide:  # so will_divide == True
-                dividing_cells.append(previous_cell_cycle_length)
-            else:  # will_divide == False
-                nondividing_cells.append(previous_cell_cycle_length)
-    return dividing_cells, nondividing_cells, unknown_fate_cells
+    x_start = time_point_numbers[0]
+    x_end = time_point_numbers[-1] + time_points_per_bin + 1
+    axes.set_xticks(range(x_start, x_end, time_points_per_bin))
 
 
 def _will_divide(graph: Graph, particle: Particle, max_time_point_number: int) -> Optional[bool]:
@@ -78,3 +76,59 @@ def _will_divide(graph: Graph, particle: Particle, max_time_point_number: int) -
             return None  # We are not sure
         return False  # Cell with not divide
     return True  # Cell will divide in the future
+
+
+class _Bin:
+    """A single bin in the histogram."""
+    _dividing_count: int = 0
+    _nondividing_count: int = 0
+    _unknown_count: int = 0
+
+    min_time_point_number: int
+
+    def __init__(self, min_time_point_number: int):
+        self.min_time_point_number = min_time_point_number
+
+    def dividing_fraction(self):
+        return self._dividing_count / self.total_number_of_cells()
+
+    def nondividing_fraction(self):
+        return self._nondividing_count / self.total_number_of_cells()
+
+    def unknown_fraction(self):
+        return self._unknown_count / self.total_number_of_cells()
+
+    def total_number_of_cells(self):
+        return self._dividing_count + self._nondividing_count + self._unknown_count
+
+    def add_data_point(self, will_divide: Optional[bool]):
+        if will_divide is None:
+            self._unknown_count += 1
+        elif will_divide:
+            self._dividing_count += 1
+        else:
+            self._nondividing_count += 1
+
+    def is_empty(self):
+        return self.total_number_of_cells() == 0
+
+
+def _classify_cell_divisions(links: Graph, time_points_per_bin: int, max_time_point_number: int) -> List[_Bin]:
+    """Classifies each cell division in the data: is it the last cell division in a lineage, or will there be more after
+    this? The cell divisions are returned in bins. No empty bins are returned."""
+    bins = list()
+
+    for cell_division in mother_finder.find_families(links):
+        previous_cell_cycle_length = cell_cycle.get_age(links, cell_division.mother)
+        if previous_cell_cycle_length is None:
+            continue  # Cannot plot without knowing the length of the previous cell cycle
+
+        bin_index = previous_cell_cycle_length // time_points_per_bin  # Integer division
+        while bin_index >= len(bins):
+            bins.append(_Bin(len(bins) * time_points_per_bin))  # Resize bin array so that it will fit
+        bin = bins[bin_index]
+
+        for daughter in cell_division.daughters:
+            will_divide = _will_divide(links, daughter, max_time_point_number)
+            bin.add_data_point(will_divide)
+    return [bin for bin in bins if not bin.is_empty()]
