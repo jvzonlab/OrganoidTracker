ea5251f51d409eb8a4564365c460d9f3d0b366ad
Rutger Kok <rutger.kok@live.com>
2018-11-26 12:19:58 +0100

    Copy over Jeroens lineage tree plotting code

--- autotrack/core/resolution.py
+++ autotrack/core/resolution.py
@@ -9,3 +9,7 @@ class ImageResolution:
     def __init__(self, pixel_size_x_um: float, pixel_size_y_um: float, pixel_size_z_um: float, time_point_interval_m: float):
         self.pixel_size_zyx_um = (pixel_size_z_um, pixel_size_y_um, pixel_size_x_um)
         self.time_point_interval_m = time_point_interval_m
+
+    @property
+    def time_point_interval_h(self) -> float:
+        return self.time_point_interval_m / 60
