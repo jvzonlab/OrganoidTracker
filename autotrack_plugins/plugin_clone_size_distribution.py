2985ee99ddeafe7d34db81f1d9b0ee61a8dae115
Rutger Kok <rutger.kok@live.com>
2018-12-10 15:43:29 +0100

    Move lineage division count function to another module

--- autotrack_plugins/plugin_clone_size_distribution.py
+++ autotrack_plugins/plugin_clone_size_distribution.py
@@ -11,6 +11,7 @@ from autotrack.gui.gui_experiment import GuiExperiment
 from autotrack.gui.threading import Task
 from autotrack.gui.window import Window
 from autotrack.linking_analysis import linking_markers
+from autotrack.linking_analysis.lineage_division_count import get_division_count_in_lineage
 from autotrack.linking_analysis.linking_markers import EndMarker
 
 _LINEAGE_FOLLOW_TIME_H = 35
@@ -73,30 +74,15 @@ class _CloneDistributionTask(Task):
 def _get_clone_sizes_list(links: ParticleLinks, time_point_window: int, first_time_point_number: int, last_time_point_number: int) -> ndarray:
     clone_sizes = list()
     for view_start_time_point in range(first_time_point_number, last_time_point_number - time_point_window, 5):
-        print("Calculating clone sizes at time point", view_start_time_point)
+        # Process a single time point window, from view_start_time_point to view_end_time_point
         view_end_time_point = view_start_time_point + time_point_window
+
         for track in links.find_all_tracks_in_time_point(view_start_time_point):
-            division_count = _get_division_count_in_lineage(track, links, view_end_time_point)
+            # Process a single lineage in that time point
+            division_count = get_division_count_in_lineage(track, links, view_end_time_point)
             if division_count is not None and division_count > 0:
                 clone_sizes.append(division_count + 1)
     return numpy.array(clone_sizes, dtype=numpy.int32)
-
-
-def _get_division_count_in_lineage(starting_track: LinkingTrack, links: ParticleLinks, last_time_point_number: int) -> Optional[int]:
-    """Gets how many divisions there are in the lineage starting at the given cell. If the cell does not divide, then
-    this method will return 0."""
-    division_count = 0
-    for track in starting_track.find_all_descending_tracks(include_self=True):
-        if track.min_time_point_number() > last_time_point_number:
-            # Ignore this track, it is past the time window
-            continue
-        if not track.get_next_tracks() \
-                and linking_markers.get_track_end_marker(links, track.find_last_particle()) != EndMarker.DEAD\
-                and track.max_time_point_number() < last_time_point_number:
-            return None  # Don't know why this track ended, division count in lineage is uncertain
-        if track.max_time_point_number() < last_time_point_number and len(track.get_next_tracks()) > 1:
-            division_count += 1
-    return division_count
 
 
 def _draw_clone_sizes(figure: Figure, clone_sizes: ndarray):
