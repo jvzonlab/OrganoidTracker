4780f601377181a2b82d3deab324595d0a1e977c
Rutger Kok <rutger.kok@live.com>
2018-12-10 17:14:05 +0100

    Support submenus

--- autotrack/gui/window.py
+++ autotrack/gui/window.py
@@ -1,6 +1,6 @@
-from typing import Callable, Dict, List, Any, Optional
+from typing import Callable, Dict, List, Any, Optional, Union, Iterable, Tuple
 
-from PyQt5.QtWidgets import QMainWindow, QLabel, QMenuBar, QAction
+from PyQt5.QtWidgets import QMainWindow, QLabel, QMenuBar, QAction, QMenu
 from matplotlib.figure import Figure
 
 from autotrack.core.experiment import Experiment
@@ -126,24 +126,82 @@ class Window:
         pass
 
 
-def _simple_menu_dict_to_nested(menu_items: Dict[str, Any]) -> Dict[str, Dict[str, Any]]:
-    menu_tree = {   # Forced order of base menus - these must go first
-        "File": {}, "Edit": {}, "View": {}
-    }
+_NEW_CATEGORY = "---"
+
+
+class _MenuData:
+
+    _categories: Dict[str, Dict[str, Union[Callable, "_MenuData"]]]
+
+    def __init__(self, *starting_names: str):
+        """Initializes a menu. Use starting_names to fix the order of the submenus with the given names."""
+        self._categories = dict()
+        if starting_names:
+            self._categories[""] = dict()
+            for name in starting_names:
+                self._categories[""][name] = _MenuData()
+
+    def items(self) -> Iterable[Tuple[str, Union[Callable, "_MenuData", None]]]:
+        """Gets all menu items from any category. Yields (_NEW_CATEGORY, None) in between two categories."""
+        first_category = True
+        for category in self._categories.values():
+            if first_category:
+                first_category = False
+            else:
+                yield _NEW_CATEGORY, None
+            yield from category.items()
+
+    def is_empty(self):
+        return len(self._categories) == 0
+
+    def to_qmenu(self, qmenu: QMenu):
+        for item_label, item_action in self.items():
+            if item_label == _NEW_CATEGORY:  # Separation line
+                qmenu.addSeparator()
+            elif isinstance(item_action, _MenuData):  # Submenu
+                sub_menu = qmenu.addMenu(item_label)
+                item_action.to_qmenu(sub_menu)
+            else:  # Single action
+                action = QAction(item_label, qmenu)
+                action.triggered.connect(_with_safeguard(item_action))
+                qmenu.addAction(action)
+
+    def add_menu_entry(self, name: str, action: Callable):
+        """Examples:
+
+            self.add_menu_entry("Lineages-Show lineage tree...", _show_lineage_tree)
+
+            self.add_menu_entry("Analysis-Cell deaths/Distance-Distance between neighbor cells...", _show_distances)
+        """
+        split_by_slashes = name.split("/")
+        part_for_this_menu = split_by_slashes[0]
+
+        if "-" in part_for_this_menu:
+            category_name, label = name.split("-", maxsplit=2)
+        else:
+            category_name, label = "", part_for_this_menu
+
+        # Add new category if it doesn't exist yet
+        if category_name not in self._categories:
+            self._categories[category_name] = dict()
+
+        if len(split_by_slashes) > 1:
+            # Add to sub menu
+            category = self._categories[category_name]
+            if label not in category:
+                category[label] = _MenuData()
+            sub_menu: _MenuData = category[label]
+            sub_menu.add_menu_entry("/".join(split_by_slashes[1:]), action)
+        else:
+            # Add to this menu
+            self._categories[category_name][label] = action
+
+
+def _simple_menu_dict_to_nested(menu_items: Dict[str, Any]) -> _MenuData:
+    menu_tree = _MenuData("File", "Edit", "View")  # Force order of standard menus
 
     for name, action in menu_items.items():
-        slash_index = name.index("/")
-        main_menu_name, sub_name = name[0:slash_index], name[slash_index + 1:]
-        dash_index = sub_name.index("-")
-        category_name, label = sub_name[0:dash_index], sub_name[dash_index + 1:]
-
-        if main_menu_name not in menu_tree:
-            menu_tree[main_menu_name] = dict()
-        categories = menu_tree[main_menu_name]
-        if category_name not in categories:
-            categories[category_name] = dict()
-        category = categories[category_name]
-        category[label] = action
+        menu_tree.add_menu_entry(name, action)
 
     return menu_tree
 
@@ -155,20 +213,10 @@ def _update_menu(q_window: QMainWindow, menu_bar: QMenuBar, menu_items: Dict[str
 
     for menu_name, dropdown_items in menu_tree.items():
         # Create each dropdown menu
-        if not dropdown_items:
+        if not isinstance(dropdown_items, _MenuData) or dropdown_items.is_empty():
             continue  # Ignore empty menus
-        menu = menu_bar.addMenu(menu_name)
-        first_category = True
-        for category_items in dropdown_items.values():
-            if not first_category:
-                menu.addSeparator()
-            else:
-                first_category = False
-
-            for item_name, item_action in category_items.items():
-                action = QAction(item_name, q_window)
-                action.triggered.connect(_with_safeguard(item_action))
-                menu.addAction(action)
+        qmenu = menu_bar.addMenu(menu_name)
+        dropdown_items.to_qmenu(qmenu)
 
 
 def _with_safeguard(action):
