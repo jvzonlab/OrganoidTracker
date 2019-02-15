fd99d318bad7aae4667a8eba21f2e15accfc10e7
Widya Puspitaloka <w.puspitaloka@amolf.nl>
2019-02-15 11:41:09 +0100

    Add lineage for small distance

--- tests/autotrack_departing_daughter.py
+++ tests/autotrack_departing_daughter.py
@@ -7,26 +7,31 @@ from autotrack.core import TimePoint
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 # Loading a new experiment from existing data
 experiment = io.load_data_file(
     "S:/AMOLF/groups/zon-group/guizela/multiphoton/organoids/17-07-28_weekend_H2B-mCherry/nd799xy20-stacks/Automatic analysis/31-1_correctdata.aut")
 
-#print(experiment.last_time_point_number())
-
+# Store mothers cell
 mothers = cell_division_finder.find_mothers(experiment.links)
-# print (mothers)
 
-# .. Make an empty list here
+# Empty list for distance
 distance_list = []
+distance_36min_list = []
 
+# Get position and distance for every mother cells and their daughters
 for mother in mothers:
-    #print("Looking at mother", mother)
     daughter1, daughter2 = experiment.links.find_futures(mother)
-    #print("Has daughters", daughter1, daughter2)
     distance = daughter1.distance_squared(daughter2)
-    #print(distance)
-    #print(daughter1, daughter2)
+    distance_sqrt = math.sqrt(distance)
+    # Check lineage for distance less than 35
+    if distance_sqrt < 35:
+        track_1 = experiment.links.get_track(daughter1)
+        track_2 = experiment.links.get_track(daughter2)
+        print("Track goes from time point", track_1.min_time_point_number(), track_2.min_time_point_number(), "to",
+              track_1.max_time_point_number(), track_2.max_time_point_number(), "after which",
+              len(track_1.get_next_tracks()), len(track_2.get_next_tracks()),
+              "directly follow")
+    # Get position and distance of daughter cells if the distance less than  200*2, and only execute when it is 0 or more than 1
     while distance < 200**2:
         next_daughters1 = experiment.links.find_futures(daughter1)
         next_daughters2 = experiment.links.find_futures(daughter2)
@@ -35,21 +40,20 @@ for mother in mothers:
         daughter1 = next_daughters1.pop()
         daughter2 = next_daughters2.pop()
         distance = daughter1.distance_squared(daughter2)
-        distance_sqrt = math.sqrt(distance)
-        #print(daughter1, daughter2, distance)
-        # mother.time_point_number()
-        if daughter1.time_point_number() == mother.time_point_number() + 1:
+        # Compare the distance of daughter cells in different time point
+        if daughter1.time_point_number() == mother.time_point_number() + 10:
             distance_list.append(distance_sqrt)
-           # if (distance_sqrt) < 25:
-               # print('mother',mother,'daughter',daughter1, daughter2,'with distance:', distance_sqrt)
-            #else:
-               # print('sisters are large')
+            distance_36min_list.append(distance_36mins)
+        if daughter1.time_point_number() == mother.time_point_number() +3:
+            distance_36mins = distance_sqrt
 
-    #print('end of mother cell')
+
+
 
 # .. Loop has ended, now our list is complete
-plt.hist(distance_list)
-plt.suptitle('Sister cells distances after 108 mins')
-plt.xlabel('distance between sister cells after 108 mins')
-plt.ylabel('number of cases')
-plt.show()
+# Make plot for distance comparison
+plt.scatter(distance_list, distance_36min_list)
+plt.xlabel('distance between sister cells after 2 hours')
+plt.ylabel('distance between sister cells after 36 mins')
+plt.suptitle('Sister cells distances after 2 hours v. 36 mins')
+#plt.show()
