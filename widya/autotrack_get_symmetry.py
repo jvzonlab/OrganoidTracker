e6f9b65428046e1afd346562fe68b206c069bb46
Rutger Kok <rutger.kok@live.com>
2019-03-29 16:07:32 +0100

    Fix get_symmetry looiking at last position of track for determining fate

--- widya/autotrack_get_symmetry.py
+++ widya/autotrack_get_symmetry.py
@@ -4,13 +4,13 @@ from autotrack.linking_analysis.cell_fate_finder import get_fate, CellFateType
 
 
 def get_symmetry(experiment: Experiment, track_1: LinkingTrack, track_2: LinkingTrack):
-    fate_1 = get_fate(experiment, track_1.find_last_position())
-    fate_2 = get_fate(experiment, track_2.find_last_position())
+    fate_1 = get_fate(experiment, track_1.find_first_position())
+    fate_2 = get_fate(experiment, track_2.find_first_position())
     if fate_1.type == CellFateType.WILL_DIVIDE and fate_2.type == CellFateType.WILL_DIVIDE:
         # Both are dividing
         return True
-    if fate_1.type == CellFateType.WILL_DIVIDE or (fate_1.type == CellFateType.WILL_DIE or fate_1.type == CellFateType.WILL_SHED)\
-            and fate_2.type == CellFateType.WILL_DIVIDE or (fate_2.type == CellFateType.WILL_DIE or fate_2.type == CellFateType.WILL_SHED):
+    if (fate_1.type == CellFateType.WILL_DIVIDE or fate_1.type == CellFateType.WILL_DIE or fate_1.type == CellFateType.WILL_SHED)\
+            and (fate_2.type == CellFateType.WILL_DIVIDE or fate_2.type == CellFateType.WILL_DIE or fate_2.type == CellFateType.WILL_SHED):
         # One dead or shed, one divide
         return True
     if fate_1.type == CellFateType.JUST_MOVING and fate_2.type == CellFateType.JUST_MOVING:
@@ -18,5 +18,5 @@ def get_symmetry(experiment: Experiment, track_1: LinkingTrack, track_2: Linking
         return True
     if fate_1.type == CellFateType.UNKNOWN or fate_2.type == CellFateType.UNKNOWN:
         # Don't know the fate of one of the cells
-        return False
+        return True
     return False
