6f8d99b6eec31814f5aee2b0f1a66dcc981990cf
Widya Puspitaloka <w.puspitaloka@amolf.nl>
2019-04-02 11:28:33 +0200

    Mother local cell density updated

--- widya/autotrack_local_cell_density.py
+++ widya/autotrack_local_cell_density.py
@@ -10,7 +10,6 @@ import numpy as np
 import math
 
 
-
 # Loading a new experiment from existing data
 experiments =[
     io.load_data_file("S:/AMOLF/groups/zon-group/guizela/multiphoton/organoids/17-07-28_weekend_H2B-mCherry/nd799xy20-stacks/Automatic analysis/15-3_with_axis.aut"),
@@ -20,9 +19,8 @@ experiments =[
     io.load_data_file("S:/AMOLF/groups/zon-group/guizela/multiphoton/organoids/17-07-28_weekend_H2B-mCherry/nd799xy08-stacks/analyzed/lineages.p")
 ]
 
-mother_pos_axis = []
 
-for experiment in experiments:
+for experiment_number, experiment in enumerate(experiments):
     # Store mothers cell
     mothers = cell_division_finder.find_mothers(experiment.links)
 
@@ -31,6 +29,7 @@ for experiment in experiments:
         # get the distance of mother to the axis
         pos_axis = experiment.data_axes.to_position_on_original_axis(experiment.links, mother).pos
         distance_um = experiment.images.resolution().pixel_size_x_um * pos_axis
+        mother_pos_axis = []
         mother_pos_axis.append(distance_um)
 
         # find fix number of closest cells to mother cells
@@ -50,12 +49,14 @@ for experiment in experiments:
         average_distance_nearby_cells = np.average(nearby_mother_cell)
         average_nearby_cells_mother_um = experiment.images.resolution().pixel_size_x_um * average_distance_nearby_cells
         average_mother_nearby.append(average_nearby_cells_mother_um)
+
         #count_1 = count_1 + 1
         #print(count_1, average_mother_nearby)
 
-        plt.rcParams["font.family"] = "arial"
-        plt.plot(average_mother_nearby, color ='lightblue')
-        plt.xlabel('Distance to crypt axis')
-        plt.ylabel('Cell density (μm)')
-        plt.suptitle('Mother Cells Positions in Crypt-Villus Axis')
-        plt.show()
+
+plt.rcParams["font.family"] = "arial"
+plt.scatter(mother_pos_axis, average_mother_nearby)
+plt.xlabel('Distance to crypt axis')
+plt.ylabel('Cell density (μm)')
+plt.suptitle('Cell Density along the Crypt-Villus Axis')
+plt.show()
