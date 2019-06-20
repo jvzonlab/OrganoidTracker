1a346adb4d9b3df79172f407a8e90f5ff8607087
Rutger Kok <rutger.kok@live.com>
2019-06-20 11:52:44 +0200

    Add plugin to show cell densities for shed cells in the villus

--- autotrack/util/mpl_helper.py
+++ autotrack/util/mpl_helper.py
@@ -4,3 +4,7 @@ from matplotlib import colors
 # Source: flatuicolors.com - US palette
 SANDER_APPROVED_COLORS = colors.to_rgba_array({"#55efc4", "#fdcb6e", "#636e72", "#74b9ff", "#e84393",
                                                "#ff7675", "#fd79a8", "#fab1a0", "#e84393"})
+
+# A bit darker colors
+BAR_COLOR_1 = "#0984e3"
+BAR_COLOR_2 = "#d63031"
