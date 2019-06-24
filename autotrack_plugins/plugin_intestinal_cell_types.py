99fbf1bfd51ace0767707fe5c361d242bb2b641c
Rutger Kok <rutger.kok@live.com>
2019-06-24 13:23:25 +0200

    Add more cell types

--- autotrack_plugins/plugin_intestinal_cell_types.py
+++ autotrack_plugins/plugin_intestinal_cell_types.py
@@ -5,16 +5,22 @@ from autotrack.core.marker import Marker
 from autotrack.gui.window import Window
 
 
-STEM = Marker([Position], "STEM", "stem cell", (255, 0, 0))  # Not yet used or registered
-PANETH = Marker([Position], "PANETH", "Paneth cell", (0, 0, 255))
+STEM = Marker([Position], "STEM", "stem cell", (255, 227, 190))
+
+# Differentiated
+M_CELL = Marker([Position], "M_CELL", "M cell", (242, 109, 84))
+ENTEROCYTE = Marker([Position], "ENTEROCYTE", "enterocyte cell", (251, 175, 64))
+PANETH = Marker([Position], "PANETH", "Paneth cell", (63, 172, 225))
+GOBLET = Marker([Position], "GOBLET", "Goblet cell", (147, 190, 147))
+ENTEROENDOCRINE = Marker([Position], "ENTEROENDOCRINE", "enteroendocrine cell", (239, 212, 202))
+TUFT = Marker([Position], "TUFT", "Tuft cell", (101, 46, 143))
 
 CRYPT = Marker([Spline], "CRYPT", "crypt axis", (255, 0, 0), is_axis=True)
-VILLUS = Marker([Spline], "VILLUS", "villus region", (255, 0, 0), is_axis=False)
 
 
 def init(window: Window):
     gui_experiment = window.get_gui_experiment()
-    gui_experiment.register_marker(PANETH)
+    for marker in [STEM, M_CELL, ENTEROCYTE, PANETH, GOBLET, ENTEROENDOCRINE, TUFT]:
+        gui_experiment.register_marker(marker)
 
     gui_experiment.register_marker(CRYPT)
-    gui_experiment.register_marker(VILLUS)
