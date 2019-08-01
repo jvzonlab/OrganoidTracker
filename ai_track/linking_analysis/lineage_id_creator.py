043b9e6ecc0355238ec4d7f4754886f68cf18383
Rutger Kok <rutger.kok@live.com>
2019-08-01 10:44:15 +0200

    Make lineage ids more random

--- ai_track/linking_analysis/lineage_id_creator.py
+++ ai_track/linking_analysis/lineage_id_creator.py
@@ -1,6 +1,15 @@
 """Assigns an id to an lineage tree."""
-from ai_track.core.links import Links
+import random
+from typing import Dict
+
+import numpy
+
+from ai_track.core.links import Links, LinkingTrack
 from ai_track.core.position import Position
+
+# Used for randomization of last three digits
+_RANDOM_LIST = list(range(2000))
+random.Random(12).shuffle(_RANDOM_LIST)
 
 
 def get_lineage_id(links: Links, position: Position) -> int:
@@ -20,4 +29,11 @@ def get_lineage_id(links: Links, position: Position) -> int:
 
     if not in_a_tree:
         return -1
-    return links.get_track_id(track)
+
+    track_id = links.get_track_id(track)
+
+    # Randomize last three digits
+    track_id_randomized = (track_id // len(_RANDOM_LIST)) * len(_RANDOM_LIST) \
+                          + _RANDOM_LIST[track_id % len(_RANDOM_LIST)]
+
+    return track_id_randomized
