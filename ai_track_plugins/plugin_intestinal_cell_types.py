e4b39c523a0361b3f253a9229494129b227acd50
Rutger Kok <rutger.kok@live.com>
2019-08-13 09:01:13 +0200

    Allow lumen tracking

--- ai_track_plugins/plugin_intestinal_cell_types.py
+++ ai_track_plugins/plugin_intestinal_cell_types.py
@@ -10,6 +10,7 @@ STEM = Marker([Position], "STEM", "stem cell", (255, 227, 190))
 # Differentiated
 M_CELL = Marker([Position], "M_CELL", "M cell", (242, 109, 84))
 ENTEROCYTE = Marker([Position], "ENTEROCYTE", "enterocyte cell", (251, 175, 64))
+LUMEN = Marker([Position], "LUMEN", "lumen", (30, 30, 30))
 PANETH = Marker([Position], "PANETH", "Paneth cell", (63, 172, 225))
 GOBLET = Marker([Position], "GOBLET", "Goblet cell", (147, 190, 147))
 ENTEROENDOCRINE = Marker([Position], "ENTEROENDOCRINE", "enteroendocrine cell", (239, 212, 202))
@@ -20,7 +21,7 @@ CRYPT = Marker([Spline], "CRYPT", "crypt axis", (255, 0, 0), is_axis=True)
 
 def init(window: Window):
     gui_experiment = window.get_gui_experiment()
-    for marker in [STEM, M_CELL, ENTEROCYTE, PANETH, GOBLET, ENTEROENDOCRINE, TUFT]:
+    for marker in [STEM, M_CELL, ENTEROCYTE, LUMEN, PANETH, GOBLET, ENTEROENDOCRINE, TUFT]:
         gui_experiment.register_marker(marker)
 
     gui_experiment.register_marker(CRYPT)
