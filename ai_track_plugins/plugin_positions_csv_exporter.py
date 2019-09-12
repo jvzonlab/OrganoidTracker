43d44364d2e2979a215215efdad3a0cefb7db88b
Rutger Kok <rutger.kok@live.com>
2019-09-12 08:52:39 +0200

    Export cell types in CSV output

--- ai_track_plugins/plugin_positions_csv_exporter.py
+++ ai_track_plugins/plugin_positions_csv_exporter.py
@@ -1,16 +1,18 @@
 import os
-from typing import Tuple, Dict, Any
+from typing import Tuple, Dict, Any, Iterable, List
 
 from ai_track.core import UserError
 from ai_track.core.experiment import Experiment
 
 from ai_track.core.links import Links
+from ai_track.core.marker import Marker
+from ai_track.core.position import Position
 from ai_track.core.position_collection import PositionCollection
 from ai_track.core.resolution import ImageResolution
 from ai_track.gui import dialog
 from ai_track.gui.threading import Task
 from ai_track.gui.window import Window
-from ai_track.position_analysis import cell_curvature_calculator
+from ai_track.linking_analysis import linking_markers
 
 
 def get_menu_items(window: Window) -> Dict[str, Any]:
@@ -30,7 +32,8 @@ def _export_positions_as_csv(window: Window, *, metadata: bool):
         return
     os.mkdir(folder)
     if metadata:
-        window.get_scheduler().add_task(_AsyncExporter(experiment, folder))
+        cell_types = list(window.get_gui_experiment().get_registered_markers(Position))
+        window.get_scheduler().add_task(_AsyncExporter(experiment, cell_types, folder))
     else:
         _write_positions_to_csv(experiment, folder)
         _export_help_file(folder)
@@ -59,6 +62,18 @@ You will now end up with a nice 3D view of the detected points. To save a movie,
         file_handle.write(text)
 
 
+def _export_cell_types_file(folder: str, cell_types: List[Marker]):
+    """Writes all known cell types and their ids to a file."""
+    if len(cell_types) == 0:
+        return
+    file_name = os.path.join(folder, "Known cell types.txt")
+    with open(file_name, "w") as file_handle:
+        file_handle.write("Known cell types:\n")
+        file_handle.write("=================\n")
+        for cell_type in cell_types:
+            file_handle.write("* " + cell_type.display_name + " = " + str(hash(cell_type.save_name)))
+
+
 def _write_positions_to_csv(experiment: Experiment, folder: str):
     resolution = experiment.images.resolution()
     positions = experiment.positions
@@ -79,17 +94,20 @@ class _AsyncExporter(Task):
     _resolution: ImageResolution
     _folder: str
     _save_name: str
+    _cell_types: List[Marker]
 
-    def __init__(self, experiment: Experiment, folder: str):
+    def __init__(self, experiment: Experiment, cell_types: List[Marker], folder: str):
         self._positions = experiment.positions.copy()
         self._links = experiment.links.copy()
         self._resolution = experiment.images.resolution()
         self._folder = folder
         self._save_name = experiment.name.get_save_name()
+        self._cell_types = cell_types
 
     def compute(self) -> Any:
         _write_positions_and_metadata_to_csv(self._positions, self._links, self._resolution, self._folder, self._save_name)
         _export_help_file(self._folder)
+        _export_cell_types_file(self._folder, self._cell_types)
         return "done"  # We're not using the result
 
     def on_finished(self, result: Any):
@@ -105,16 +123,16 @@ def _write_positions_and_metadata_to_csv(positions: PositionCollection, links: L
     for time_point in positions.time_points():
         file_name = os.path.join(folder, file_prefix + str(time_point.time_point_number()))
         with open(file_name, "w") as file_handle:
-            file_handle.write("x,y,z,density_mm1,curvature_angle,lineage_id,original_track_id\n")
+            file_handle.write("x,y,z,density_mm1,cell_type_id,lineage_id,original_track_id\n")
             positions_of_time_point = positions.of_time_point(time_point)
             for position in positions_of_time_point:
                 lineage_id = lineage_id_creator.get_lineage_id(links, position)
                 original_track_id = lineage_id_creator.get_original_track_id(links, position)
-                curvature = cell_curvature_calculator.get_curvature_angle(positions, position, resolution)
+                cell_type_id = hash(linking_markers.get_position_type(links, position))
                 density = cell_density_calculator.get_density_mm1(positions_of_time_point, position, resolution)
 
                 vector = position.to_vector_um(resolution)
-                file_handle.write(f"{vector.x},{vector.y},{vector.z},{density},{curvature},"
+                file_handle.write(f"{vector.x},{vector.y},{vector.z},{density},{cell_type_id},"
                                   f"{lineage_id},{original_track_id}\n")
 
 
