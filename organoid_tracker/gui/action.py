1bdac28d81f023fbf2aa3acb0d2d91a034db65f5
Rutger Kok <rutger.kok@live.com>
2020-12-08 11:55:57 +0100

    Extract image selection code to separate modluel

--- organoid_tracker/gui/action.py
+++ organoid_tracker/gui/action.py
@@ -12,7 +12,6 @@ from organoid_tracker.gui.gui_experiment import GuiExperiment
 from organoid_tracker.gui.undo_redo import UndoableAction
 from organoid_tracker.gui.window import Window
 from organoid_tracker.imaging import io
-from organoid_tracker.imaging.image_file_name_pattern_finder import find_time_and_channel_pattern
 from organoid_tracker.linking_analysis import linking_markers
 from organoid_tracker.visualizer import activate
 from organoid_tracker.visualizer.empty_visualizer import EmptyVisualizer
@@ -60,58 +59,10 @@ def close_experiment(window: Window):
 
 
 def load_images(window: Window):
-    # Show an OK/cancel box, but with an INFO icon instead of a question mark
-    full_path = dialog.prompt_load_file("Select first image file", [
-        ("Single TIF or TIF series", "*.tif;*.tiff"),
-        ("Image per time point", "*.png;*.jpg;*.gif"),
-        ("LIF file", "*.lif"),
-        ("ND2 file", "*.nd2")])
-    if not full_path:
-        return  # Cancelled
-    directory, file_name = path.split(full_path)
-
-    if file_name.endswith(".lif"):
-        # LIF file loading
-        from organoid_tracker.image_loading import _lif, liffile_image_loader
-        reader = _lif.Reader(full_path)
-        series = [header.getName() for header in reader.getSeriesHeaders()]
-        series_index = option_choose_dialog.popup_list("Choose an image serie", "Choose an image serie", "Image serie:", series)
-        if series_index is not None:
-            liffile_image_loader.load_from_lif_reader(window.get_experiment().images, full_path, reader, series_index)
-            window.redraw_all()
-        return
-
-    if file_name.endswith(".nd2"):
-        # ND2 file loading
-        from organoid_tracker.image_loading import nd2file_image_loader
-        reader = nd2file_image_loader.Nd2File(full_path)
-        max_location = reader.get_location_counts()
-        location = dialog.prompt_int("Image series", f"Which image series do you want load? (1-"
-                                     f"{max_location}, inclusive)", minimum=1, maximum=max_location)
-        if location is not None:
-            loader = nd2file_image_loader.load_image_series(reader, location)
-            window.get_experiment().images.image_loader(loader)
-            window.redraw_all()
-        return
-
-    file_name_pattern = find_time_and_channel_pattern(file_name)
-    if file_name_pattern is None:
-        file_name_pattern = file_name  # Don't use a pattern if not available
-        file_name_lower = file_name.lower()
-        if file_name_lower.endswith(".tif") or file_name_lower.endswith(".tiff"):
-            # Try as TIF container
-            from organoid_tracker.image_loading import merged_tiff_image_loader
-            merged_tiff_image_loader.load_from_tif_file(window.get_experiment(), full_path)
-            window.redraw_all()
-            return
-        dialog.popup_message("Could not read file pattern", "Could not find 't01' (or similar) in the file name \"" +
-                             file_name + "\", so only one image is loaded. If you want to load a time lapse, see the"
-                             " manual for supported image formats.")
-
-    # Load and show images
-    from organoid_tracker.image_loading import folder_image_loader
-    folder_image_loader.load_images_from_folder(window.get_experiment(), directory, file_name_pattern)
-    window.redraw_all()
+    """Prompts the image loader, and loads the images into the experiment."""
+    from organoid_tracker.gui import image_series_loader_dialog
+    if image_series_loader_dialog.prompt_image_series(window.get_experiment()):
+        window.redraw_all()
 
 
 def load_tracking_data(window: Window):
---
+++ organoid_tracker/gui/image_series_loader_dialog.py
@@ -0,0 +1,58 @@
+from organoid_tracker.core.experiment import Experiment
+from organoid_tracker.gui import dialog, option_choose_dialog
+from organoid_tracker.imaging.image_file_name_pattern_finder import find_time_and_channel_pattern
+import os.path
+
+
+def prompt_image_series(experiment: Experiment) -> bool:
+    """Prompts an image series, and loads it into the experiment. Returns whether anything was loaded."""
+    full_path = dialog.prompt_load_file("Select first image file", [
+        ("Single TIF or TIF series", "*.tif;*.tiff"),
+        ("Image per time point", "*.png;*.jpg;*.gif"),
+        ("LIF file", "*.lif"),
+        ("ND2 file", "*.nd2")])
+    if not full_path:
+        return False  # Cancelled
+    directory, file_name = os.path.split(full_path)
+
+    if file_name.endswith(".lif"):
+        # LIF file loading
+        from organoid_tracker.image_loading import _lif, liffile_image_loader
+        reader = _lif.Reader(full_path)
+        series = [header.getName() for header in reader.getSeriesHeaders()]
+        series_index = option_choose_dialog.popup_list("Choose an image serie", "Choose an image serie", "Image serie:", series)
+        if series_index is not None:
+            liffile_image_loader.load_from_lif_reader(experiment.images, full_path, reader, series_index)
+            return True
+        return False
+
+    if file_name.endswith(".nd2"):
+        # ND2 file loading
+        from organoid_tracker.image_loading import nd2file_image_loader
+        reader = nd2file_image_loader.Nd2File(full_path)
+        max_location = reader.get_location_counts()
+        location = dialog.prompt_int("Image series", f"Which image series do you want load? (1-"
+                                     f"{max_location}, inclusive)", minimum=1, maximum=max_location)
+        if location is not None:
+            loader = nd2file_image_loader.load_image_series(reader, location)
+            experiment.images.image_loader(loader)
+            return True
+        return False
+
+    file_name_pattern = find_time_and_channel_pattern(file_name)
+    if file_name_pattern is None:
+        file_name_pattern = file_name  # Don't use a pattern if not available
+        file_name_lower = file_name.lower()
+        if file_name_lower.endswith(".tif") or file_name_lower.endswith(".tiff"):
+            # Try as TIF container
+            from organoid_tracker.image_loading import merged_tiff_image_loader
+            merged_tiff_image_loader.load_from_tif_file(experiment, full_path)
+            return True
+        dialog.popup_message("Could not read file pattern", "Could not find 't01' (or similar) in the file name \"" +
+                             file_name + "\", so only one image is loaded. If you want to load a time lapse, see the"
+                             " manual for supported image formats.")
+
+    # Load and show images
+    from organoid_tracker.image_loading import folder_image_loader
+    folder_image_loader.load_images_from_folder(experiment, directory, file_name_pattern)
+    return True
