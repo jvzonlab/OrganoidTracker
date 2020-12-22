8988490cbc9b92e3bf18912c71ed11a4bd36f580
Rutger Kok <rutger.kok@live.com>
2020-12-22 16:38:29 +0100

    Let dividing cell count plugin also support multiple experiments

--- organoid_tracker_plugins/plugin_count_dividing_cells.py
+++ organoid_tracker_plugins/plugin_count_dividing_cells.py
@@ -1,12 +1,15 @@
 from typing import List
 
+from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 
-from organoid_tracker.core import UserError
+from organoid_tracker.core import Color
+from organoid_tracker.core.experiment import Experiment
 from organoid_tracker.gui import dialog
 from organoid_tracker.gui.window import Window
 from organoid_tracker.linking_analysis import cell_fate_finder, linking_markers
 from organoid_tracker.linking_analysis.cell_fate_finder import CellFate, CellFateType
+from organoid_tracker.util.mpl_helper import SANDER_APPROVED_COLORS
 
 
 def get_menu_items(window: Window):
@@ -20,57 +23,74 @@ def get_menu_items(window: Window):
 
 def _show_number_of_dividing_cells(window: Window):
     """Shows the number of dividing cells."""
-    experiment = window.get_experiment()
-    if not experiment.links.has_links():
-        raise UserError("No linking data", "No linking data is found. Cannot plot anything.")
-    resolution = experiment.images.resolution()
+    dividing_cells = []
+    for experiment in window.get_active_experiments():
+        dividing_cells.append(_DividingCells(experiment))
 
-    time_point_hours = []
-    dividing_counts_min = []
-    dividing_counts_max = []
-    paneth_counts = []
-    total_counts = []
-
-    for time_point in experiment.time_points():
-        dividing_count_min = 0
-        dividing_count_max = 0
-        paneth_count = 0
-        total_count = 0
-        for position in experiment.positions.of_time_point(time_point):
-            fate = cell_fate_finder.get_fate(experiment, position)
-            if fate.type == CellFateType.WILL_DIVIDE:
-                dividing_count_min += 1
-                dividing_count_max += 1
-            elif fate.type == CellFateType.UNKNOWN:
-                dividing_count_max += 1  # Could be dividing, but we're not sure
-
-            cell_type = linking_markers.get_position_type(experiment.position_data, position)
-            if cell_type == "PANETH":
-                paneth_count += 1
-
-            total_count += 1
-
-        time_point_hours.append(time_point.time_point_number() * resolution.time_point_interval_h)
-        dividing_counts_min.append(dividing_count_min)
-        dividing_counts_max.append(dividing_count_max)
-        paneth_counts.append(paneth_count)
-        total_counts.append(total_count)
-
-    dialog.popup_figure(window.get_gui_experiment(), lambda figure: _draw_figure(figure, time_point_hours,
-                                                                                 dividing_counts_min,
-                                                                                 dividing_counts_max,
-                                                                                 paneth_counts,
-                                                                                 total_counts))
+    dialog.popup_figure(window.get_gui_experiment(), lambda figure: _draw_figure(figure, dividing_cells))
 
 
-def _draw_figure(figure: Figure, time_point_hours: List[int], dividing_counts_min: List[int],
-                 dividing_counts_max: List[int], paneth_counts: List[int], total_counts: List[int]):
-    axes = figure.gca()
+class _DividingCells:
+    experiment_color: Color
+    time_point_hours: List[float]
+    dividing_counts_min: List[int]
+    dividing_counts_max: List[int]
+    paneth_counts: List[int]
+    total_counts: List[int]
+
+    def __init__(self, experiment: Experiment):
+        self.experiment_color = experiment.color
+        self.time_point_hours = []
+        self.dividing_counts_min = []
+        self.dividing_counts_max = []
+        self.paneth_counts = []
+        self.total_counts = []
+
+        resolution = experiment.images.resolution()
+        for time_point in experiment.time_points():
+            dividing_count_min = 0
+            dividing_count_max = 0
+            paneth_count = 0
+            total_count = 0
+            for position in experiment.positions.of_time_point(time_point):
+                fate = cell_fate_finder.get_fate(experiment, position)
+                if fate.type == CellFateType.WILL_DIVIDE:
+                    dividing_count_min += 1
+                    dividing_count_max += 1
+                elif fate.type == CellFateType.UNKNOWN:
+                    dividing_count_max += 1  # Could be dividing, but we're not sure
+
+                cell_type = linking_markers.get_position_type(experiment.position_data, position)
+                if cell_type == "PANETH":
+                    paneth_count += 1
+
+                total_count += 1
+
+            self.time_point_hours.append(time_point.time_point_number() * resolution.time_point_interval_h)
+            self.dividing_counts_min.append(dividing_count_min)
+            self.dividing_counts_max.append(dividing_count_max)
+            self.paneth_counts.append(paneth_count)
+            self.total_counts.append(total_count)
+
+
+def _draw_figure(figure: Figure, dividing_cells: List[_DividingCells]):
+    axes: Axes = figure.gca()
     axes.set_title("Number of dividing cells over time")
-    axes.plot(time_point_hours, dividing_counts_min, label="Dividing cells")
-    axes.fill_between(time_point_hours, dividing_counts_min, dividing_counts_max)
-    axes.plot(time_point_hours, paneth_counts, color="red", label="Paneth cells")
-    axes.plot(time_point_hours, total_counts, color="black", label="All cells")
-    axes.set_ylabel("Number of cells (min and max)")
+    if len(dividing_cells) == 1:
+        single_experiment = dividing_cells[0]
+        axes.plot(single_experiment.time_point_hours, single_experiment.dividing_counts_min, label="Dividing cells")
+        axes.fill_between(single_experiment.time_point_hours, single_experiment.dividing_counts_min,
+                          single_experiment.dividing_counts_max)
+        axes.plot(single_experiment.time_point_hours, single_experiment.paneth_counts, color="red",
+                  label="Paneth cells")
+        axes.plot(single_experiment.time_point_hours, single_experiment.total_counts, color="black", label="All cells")
+        axes.set_ylabel("Number of cells (min and max)")
+        axes.legend()
+    else:
+        for i, single_experiment in enumerate(dividing_cells):
+            color = single_experiment.experiment_color
+            axes.plot(single_experiment.time_point_hours, single_experiment.dividing_counts_min, color=color)
+            axes.plot(single_experiment.time_point_hours, single_experiment.paneth_counts, color=color, linestyle='dashed')
+        axes.set_ylabel("Number of cells (dividing and Paneth)")
     axes.set_xlabel("Time (h)")
-    axes.legend()
+
