e4d586f20f83f67b31507e0af118fd10365ab876
Rutger Kok <rutger.kok@live.com>
2021-03-23 10:44:09 +0100

    Add class for calculating the average line
    Useful for plotting various kinds of measures of cells.

--- organoid_tracker/util/moving_average.py
+++ organoid_tracker/util/moving_average.py
@@ -1,11 +1,26 @@
-from typing import List, Union
+from abc import abstractmethod, ABC
+from typing import List, Union, Tuple
 
 import numpy
 from matplotlib.axes import Axes
 from numpy import ndarray
 
+from organoid_tracker.core import min_none, max_none
 
-class MovingAverage:
+
+class PlotAverage(ABC):
+
+    @abstractmethod
+    def plot(self, axes: Axes, *, color="blue", linewidth=2, error_opacity=0.8, standard_error: bool = False, label="Moving average"):
+        """Plots the moving average to the given axis. For example:
+
+                    plot(plt.gca(), label="My moving average")
+
+                The error bar is either the standard deviation (if standard_error is False) or the standard error (if standard_error is True).
+                """
+
+
+class MovingAverage(PlotAverage):
     """A moving average calculation for a point cloud. Usage example:
 
     >>> import matplotlib.pyplot as plt
@@ -71,12 +86,6 @@ class MovingAverage:
         self.counts_in_standard_deviation_values = numpy.array(y_moving_average_counts, dtype=numpy.uint16)
 
     def plot(self, axes: Axes, *, color="blue", linewidth=2, error_opacity=0.8, standard_error: bool = False, label="Moving average"):
-        """Plots the moving average to the given axis. For example:
-
-            plot(plt.gca(), label="My moving average")
-
-        The error bar is either the standard deviation (if standard_error is False) or the standard error (if standard_error is True).
-        """
         axes.plot(self.x_values, self.mean_values, color=color, linewidth=linewidth, label=label)
 
         error_bar_size = self.standard_deviation_values
@@ -85,3 +94,84 @@ class MovingAverage:
 
         axes.fill_between(self.x_values, self.mean_values - error_bar_size,
                           self.mean_values + error_bar_size, color=color, alpha=1 - error_opacity)
+
+
+class LinesAverage(PlotAverage):
+    """A moving average for a bunch of lines."""
+
+    _lines: List[Tuple[List[float], List[float]]]
+    _x_step_size: float
+
+    def __init__(self, *lines: Tuple[List[float], List[float]], x_step_size: float = 1):
+        """Creates the moving average. Each line is ([x1, x2, ...], [y1, y2, ...]), with the x in order from low to high."""
+        self._lines = list(lines)
+        self._x_step_size = x_step_size
+        if x_step_size <= 0:
+            raise ValueError(f"Illegal step size: {x_step_size}")
+
+    def _get_min_max_x(self) -> Tuple[float, float]:
+        """Gets the lowest and highest x values used in the lines. Returns (0, 1) if no lines are available."""
+        min_x = None
+        max_x = None
+        for line_x, _ in self._lines:
+            min_x = min_none(line_x[0], min_x)
+            max_x = max_none(line_x[-1], max_x)
+        if min_x is None:
+            return 0, 1
+        if max_x == min_x:
+            return min_x, max_x + 1
+        return min_x, max_x
+
+    def _get_y_values_at(self, x: float) -> List[float]:
+        """Returns a list of all y values of the lines at the given position,
+        using linear interpolation if necessary. May return an empty list."""
+        y_values = []
+        for line_x, line_y in self._lines:
+            if x <= line_x[0]:
+                if x == line_x[0]:
+                    y_values.append(line_y[0])
+                continue
+            if x >= line_x[-1]:
+                if x == line_x[-1]:
+                    y_values.append(line_y[-1])
+                continue
+            for i in range(len(line_x)):
+                if x < line_x[i]:
+                    # Need to grab y value in between point i and i-1
+
+                    # i_fraction is 0 if we're at line_x[i] and 1 if at line_x[i - 1]
+                    i_fraction = (line_x[i] - x) / (line_x[i] - line_x[i - 1])
+
+                    y_value = i_fraction * line_y[i - 1] + (1 - i_fraction) * line_y[i]
+                    y_values.append(y_value)
+                    break
+        return y_values
+
+    def plot(self, axes: Axes, *, color="blue", linewidth=2, error_opacity=0.8, standard_error: bool = False, label="Moving average"):
+        min_x, max_x = self._get_min_max_x()
+
+        # Calculate error bounds
+        x_error_values = list()
+        y_error_values_min = list()
+        y_error_values_mean = list()
+        y_error_values_max = list()
+        for x in numpy.arange(min_x, max_x, self._x_step_size):
+            y_values = self._get_y_values_at(x)
+            if len(y_values) <= 1:
+                continue
+
+            y_mean = numpy.mean(y_values)
+            y_error = numpy.std(y_values, ddof=1)
+            if standard_error:
+                y_error /= numpy.sqrt(len(y_values))
+
+            x_error_values.append(x)
+            y_error_values_min.append(y_mean - y_error)
+            y_error_values_mean.append(y_mean)
+            y_error_values_max.append(y_mean + y_error)
+
+        # Plot
+        if len(x_error_values) > 0:
+            axes.plot(x_error_values, y_error_values_mean, color=color, linewidth=linewidth, label=label)
+            axes.fill_between(x_error_values, y_error_values_min,
+                          y_error_values_max, color=color, alpha=1 - error_opacity)
