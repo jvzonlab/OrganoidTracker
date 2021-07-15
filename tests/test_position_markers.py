1a7c1a0e129e5e92d0e5d2f0465d83073314289e
Rutger Kok <rutger.kok@live.com>
2021-07-15 15:39:03 +0200

    Add better intensity normalization function
    This one allows you to change the offset and multiplication factor in a single place.

---
+++ tests/test_position_markers.py
@@ -0,0 +1,20 @@
+import unittest
+
+from organoid_tracker.core.position import Position
+from organoid_tracker.core.position_data import PositionData
+from organoid_tracker.imaging import position_markers
+
+
+class TestPositionData(unittest.TestCase):
+
+
+    def test_normalization(self):
+        position_data = PositionData()
+        position_data.set_position_data(Position(0, 0, 0, time_point_number=0), "intensity", 8)
+        position_data.set_position_data(Position(1, 0, 0, time_point_number=0), "intensity", 10)
+        position_data.set_position_data(Position(2, 0, 0, time_point_number=0), "intensity", 12)
+
+        normalizer = position_markers.get_intensity_normalization(position_data)
+        self.assertEqual(50, normalizer.factor)
+        self.assertEqual(-400, normalizer.offset)
+
