ab63acc4beba426de89dde7c0b21ac2cef09f898
maxbetjes <69514180+maxbetjes@users.noreply.github.com>
2023-07-18 16:49:28 +0200

    Remove oversegmentations of divisions
    Divisions are often oversegmented. The nuclei are also much more round during division. This means that we can increase the minimum required distance between cell detection for cells undergoing division (as predicted by the neural network). This means we can remove the oversegmentations.

--- organoid_tracker_predict_divisions.py
+++ organoid_tracker_predict_divisions.py
@@ -4,6 +4,8 @@ import os
 from typing import Tuple
 
 from organoid_tracker.config import ConfigFile, config_type_bool
+from organoid_tracker.core import TimePoint
+from organoid_tracker.core.position import Position
 from organoid_tracker.image_loading.builtin_merging_image_loaders import ChannelSummingImageLoader
 from organoid_tracker.imaging import io
 from organoid_tracker.image_loading import general_image_loader
@@ -16,6 +18,8 @@ import tensorflow as tf
 import numpy as np
 
 # PARAMETERS
+from organoid_tracker.linking.nearby_position_finder import find_closest_n_positions
+
 print("Hi! Configuration file is stored at " + ConfigFile.FILE_NAME)
 config = ConfigFile("predict_divisions")
 _images_folder = config.get_or_prompt("images_container", "If you have a folder of image files, please paste the folder"
@@ -35,10 +39,16 @@ experiment = io.load_data_file(_positions_file, _min_time_point, _max_time_point
 general_image_loader.load_images(experiment, _images_folder, _images_format,
                                  min_time_point=_min_time_point, max_time_point=_max_time_point)
 
+_patch_shape_z = int(config.get_or_default("patch_shape_z", str(30), store_in_defaults=True))
+_patch_shape_y = int(config.get_or_default("patch_shape_y", str(240), store_in_defaults=True))
+_patch_shape_x = int(config.get_or_default("patch_shape_x", str(240), store_in_defaults=True))
+
 _model_folder = config.get_or_prompt("checkpoint_folder", "Please paste the path here to the \"checkpoints\" folder containing the trained model.")
 _output_file = config.get_or_default("positions_output_file", "Automatic positions.aut", comment="Output file for the positions, can be viewed using the visualizer program.")
 _channels_str = config.get_or_default("images_channels", str(1), comment="Index(es) of the channels to use. Use \"3\" to use the third channel for predictions. Use \"1,3,4\" to use the sum of the first, third and fourth channel for predictions.")
 _images_channels = {int(part) for part in _channels_str.split(",")}
+
+_min_distance_dividing = float(config.get_or_default("minimum_distance_between_dividing_cell (in micron)", str(4.5)))
 
 config.save()
 # END OF PARAMETERS
@@ -69,6 +79,10 @@ with open(os.path.join(_model_folder, "settings.json")) as file_handle:
         exit(1)
     time_window = json_contents["time_window"]
     patch_shape = json_contents["patch_shape_zyx"]
+
+    scaling = json_contents["platt_scaling"]
+    intercept = json_contents["platt_intercept"]
+    intercept = np.log10(np.exp(intercept))
 
 # load model
 print("Loading model...")
@@ -103,12 +117,79 @@ for i in range(len(image_with_positions_list)):
     positions = positions_list[i]
 
     for positions, prediction in zip(positions, predictions):
+        eps = 10 ** -10
+        likelihood = intercept + scaling * float(np.log10(prediction + eps) - np.log10(1 - prediction + eps))
+        scaled_prediction = (10**likelihood) / (1 + 10**likelihood)
+
         # add division prediction to the data
-        experiment.position_data.set_position_data(positions, data_name="division_probability", value=float(prediction))
+        experiment.position_data.set_position_data(positions, data_name="division_probability", value=float(scaled_prediction))
         # add division penalty (log-likelihood) to the data
         eps = 10 ** -10
-        experiment.position_data.set_position_data(positions, data_name="division_penalty", value=float(-np.log10(prediction+eps)+np.log10(1-prediction+eps)))
+        experiment.position_data.set_position_data(positions, data_name="division_penalty", value=float(-likelihood))
 
+# Remove oversegmentation for dividing cells by setting a minimal distance for dividing cells
+to_remove = []
+to_add = []
+
+for position in experiment.positions:
+
+    # check if cell is dividing
+    if experiment.position_data.get_position_data(position, data_name="division_probability") > 0.5:
+
+        # find 6 closest neighbors
+        neighbors = find_closest_n_positions(experiment.positions.of_time_point(position.time_point()), around = position, max_amount=6, resolution=experiment.images.resolution())
+        for neighbor in neighbors:
+
+            # check if neighbor is also dividing and if the neighbor is within the range
+            if experiment.position_data.get_position_data(neighbor, data_name="division_probability") > 0.5:
+                range = 1.5*_min_distance_dividing
+            else:
+                range = _min_distance_dividing
+
+            # if the division is oversegmented rpelace by one position in the middle
+            if (position.distance_um(neighbor, resolution=experiment.images.resolution()) < range):
+                add_position = Position(x=(position.x + neighbor.x)//2,
+                                    y=(position.y + neighbor.y)//2,
+                                    z=(position.z + neighbor.z)//2,
+                                    time_point=position.time_point())
+                to_add.append(add_position)
+
+                experiment.position_data.set_position_data(add_position, 'division_probability',
+                                                           max(experiment.position_data.get_position_data(position,
+                                                                                                          data_name="division_probability"),
+                                                               experiment.position_data.get_position_data(neighbor,
+                                                                                                          data_name="division_probability")))
+                experiment.position_data.set_position_data(add_position, 'division_penalty',
+                                                           min(experiment.position_data.get_position_data(position,
+                                                                                                          data_name="division_penalty"),
+                                                               experiment.position_data.get_position_data(neighbor,
+                                                                                                          data_name="division_penalty")))
+                to_remove = to_remove + [position, neighbor]
+
+        # find closest neighbors at previous timepoint
+        prev_time_point = TimePoint(position.time_point().time_point_number()-1)
+        neighbors = list(find_closest_n_positions(experiment.positions.of_time_point(prev_time_point), around = position, max_amount=6, resolution=experiment.images.resolution()))
+
+        if len(neighbors)>0:
+            closest_neighbor = list(find_closest_n_positions(experiment.positions.of_time_point(prev_time_point), around=position, max_amount=1,
+                                             resolution=experiment.images.resolution()))[0]
+        else:
+            closest_neighbor = None
+
+        # check for distances between cells in the previous frame
+        for neighbor in neighbors:
+            distance = position.distance_um(neighbor, resolution=experiment.images.resolution())
+
+            # remove oversegmentation in previous frame
+            if (distance < _min_distance_dividing) and (neighbor != closest_neighbor):
+                to_remove.append(neighbor)
+
+# adapt positions
+experiment.remove_positions(to_remove)
+experiment.positions.add_positions(PositionCollection(to_add))
+
+print('division oversegmentations removed:')
+print(len(to_remove))
 
 print("Saving file...")
 io.save_data_to_json(experiment, _output_file)
