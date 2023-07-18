15b6800190d1bae9bffa9a8f948990d549b6e9af
maxbetjes <69514180+maxbetjes@users.noreply.github.com>
2023-07-18 15:58:30 +0200

    Marginalalization
    Runs the marginalization procedure

---
+++ organoid_tracker/local_marginalization/local_marginalization_functions.py
@@ -0,0 +1,341 @@
+import itertools
+
+import numpy as np
+
+def minimal_marginalization(position1, position2, experiment, scale=1):
+    """Performs a marginalization on a susbet containing all the links with the same target node as the link of interest."""
+
+    if position1.time_point_number() > position2.time_point_number():
+        position1, position2 = position2, position1
+
+    # list of positions in subset
+    possible_previous = experiment.links.find_pasts(position2)
+
+    # sum of all link likelihoods (the partition function)
+    marginal = 10 ** ((-experiment.position_data.get_position_data(position2, data_name='appearance_penalty'))*scale)
+
+    for previous_pos in possible_previous:
+        marginal = marginal + 10 ** ((-experiment.link_data.get_link_data(previous_pos, position2, data_name='link_penalty'))*scale)
+
+    # actual marginalization
+    return (10 ** ((-experiment.link_data.get_link_data(position1, position2, data_name='link_penalty'))*scale)) / marginal
+
+
+def find_local_set(previous_pos, current_pos, links, steps=2, first_backward=True):
+    """Defines local subset of nodes and links based on the distance to the target node of the link of interest.
+    Link is defined by the current cell position and ist previous one.
+    Distance from the source node is used if the first step is not backward in time."""
+
+    previous_pos = {previous_pos}
+    current_pos = {current_pos}
+
+    links_out = []
+    links_in = []
+
+    i = 0
+    while i <= steps:
+        if first_backward:
+            for pos in current_pos:
+                pasts = links.find_pasts(pos)
+                previous_pos = previous_pos.union(pasts)
+
+                links_in = links_in + [pos] * len(pasts)
+                links_out = links_out + list(pasts)
+            i = i + 1
+
+        first_backward = True
+
+        if i >= steps:
+            break
+
+        for pos in previous_pos:
+            futures = links.find_futures(pos)
+            current_pos = current_pos.union(futures)
+
+            links_out = links_out + [pos] * len(futures)
+            links_in = links_in + list(futures)
+        i = i + 1
+
+    # unique links
+    local_links = set(list(zip(links_out, links_in)))
+
+    return current_pos, previous_pos, local_links
+
+
+def check_completeness(complete_pos, links):
+    """For every node we check if all the links leaving (out) or coming into (in) it are present in the subset (links)"""
+
+    complete_in = {}
+    complete_out = {}
+
+    for pos in complete_pos:
+        pasts = links.find_pasts(pos)
+        complete_in[pos] = pasts.issubset(complete_pos)
+
+        futures = links.find_futures(pos)
+        complete_out[pos] = futures.issubset(complete_pos)
+
+    return complete_in, complete_out
+
+
+
+def get_flows_and_energies(previous_pos, current_pos, local_links, experiment, integrate_ignore_penalty = True, complete_graph=True, ignore_penalty = 2):
+    """Get the allowed integer flows and associated energies for every element in the subset"""
+
+    # for every position generate unique key
+    complete_set = current_pos.union(previous_pos)
+    key_dict = dict(zip(complete_set, range(len(complete_set))))
+
+    # Characterize all possible links, (dis)appearances, and divisions
+    links_out = []
+    links_in = []
+    energies = []
+    flow = []
+    types = []
+
+    # add link energies and flows
+    for link in local_links:
+        energies.append(experiment.link_data.get_link_data(link[0], link[1], data_name='link_penalty'))
+        links_out.append(key_dict[link[0]])
+        links_in.append(key_dict[link[1]])
+        flow.append(1)
+        types.append('link')
+
+    # add energy associated with not using a position in the early timepoint
+    for pos in previous_pos:
+        if complete_graph:
+            futures = experiment.links.find_futures(pos)
+            outside_futures = futures.difference(complete_set)
+
+            outside_energies = []
+            for future in outside_futures:
+                outside_energies.append(experiment.link_data.get_link_data(pos, future, data_name='link_penalty'))
+
+            outside_energies.append(experiment.position_data.get_position_data(pos, data_name='disappearance_penalty'))
+
+            if integrate_ignore_penalty: #New element
+                outside_energies.append(ignore_penalty)
+
+            energies.append(combine_events(outside_energies))
+            #print(combine_events(outside_energies))
+
+        elif integrate_ignore_penalty:
+            combined = combine_events([ignore_penalty, experiment.position_data.get_position_data(pos, data_name='disappearance_penalty')])
+            energies.append(combined)
+        else:
+            energies.append(experiment.position_data.get_position_data(pos, data_name='disappearance_penalty'))
+
+        # these elements form self-pointers in the graph
+        links_out.append(key_dict[pos])
+        links_in.append(key_dict[pos])
+        flow.append(1)
+        types.append('previous_position')
+
+        # Add divisions
+        division_penalty = experiment.position_data.get_position_data(pos, data_name='division_penalty')
+        if division_penalty < (ignore_penalty +1):
+            energies.append(division_penalty)
+            # these elements form self-pointers in the graph
+            links_out.append(key_dict[pos])
+            links_in.append(key_dict[pos])
+            # divisions have negative flow, to allow for an extra link on that node
+            flow.append(-1)
+            types.append('division')
+
+        #if use_ignore_penalty:
+            #energies.append(ignore_penalty)
+            #links_out.append(key_dict[pos])
+            #links_in.append(None)
+            #flow.append(1)
+
+    # add energy associated with not using a position in the later timepoint
+    for pos in current_pos:
+        if complete_graph:
+            futures = experiment.links.find_pasts(pos)
+            outside_futures = futures.difference(complete_set)
+
+            outside_energies = []
+            for future in outside_futures:
+                outside_energies.append(experiment.link_data.get_link_data(pos, future, data_name='link_penalty'))
+
+            outside_energies.append(experiment.position_data.get_position_data(pos, data_name='disappearance_penalty'))
+
+            if integrate_ignore_penalty: #New element
+                outside_energies.append(ignore_penalty)
+
+            energies.append(combine_events(outside_energies))
+
+        elif integrate_ignore_penalty:
+            combined = combine_events([experiment.position_data.get_position_data(pos, data_name='appearance_penalty'), ignore_penalty])
+            energies.append(combined)
+        else:
+            energies.append(experiment.position_data.get_position_data(pos, data_name='appearance_penalty'))
+
+        links_out.append(key_dict[pos])
+        links_in.append(key_dict[pos])
+        flow.append(1)
+        types.append('current_position')
+
+        #if use_ignore_penalty:
+            #energies.append(ignore_penalty)
+            #links_out.append(None)
+            #links_in.append(key_dict[pos])
+            #flow.append(1)
+
+    return np.array(links_out), np.array(links_in), np.array(flow), np.array(energies), key_dict, np.array(types)
+
+
+def build_constraint_matrix(previous_pos, current_pos, links_out, links_in, flow,  key_dict, complete_in, complete_out, complete_graph=False):
+    """Constructs a matrix A, such that A*x=0, if x is a set of compatible flows. It also outputs per contraint if the flows have to match up exactly.
+    This does not have to be the case if not all links associated with a node are part of the subset."""
+
+    # build constraint matrix
+    constraint_matrix = [np.zeros(len(links_out))]
+    exact_match = [False]
+
+    # constraints associated with the fact that only one or two links can emerge from a single node
+    for pos in previous_pos:
+        link_out = key_dict[pos]
+        if complete_graph and (np.sum(links_out[links_in == link_out] == link_out) == 1):
+            exact_match.append(True)
+        else:
+            exact_match.append(complete_out[pos])
+        constraint_matrix.append((links_out == link_out) * flow)
+
+    # constraints associated with the fact that only one link can flow into a single node
+    for pos in current_pos:
+        if complete_graph:
+            exact_match.append(True)
+        else:
+            exact_match.append(complete_in[pos])
+        link_in = key_dict[pos]
+        constraint_matrix.append((links_in == link_in) * flow)
+
+    constraint_matrix = np.stack(constraint_matrix, axis=-1)
+    exact_match = np.array(exact_match)
+
+    return constraint_matrix, exact_match
+
+
+def construct_microstates(links_out, links_in, types, complete_graph= False):
+    """Builds all the vectors encoding possible flow states that have to checked"""
+
+    independents = []
+    options = np.eye(len(links_out))
+
+    # builds all vectors that encode a single input link per node in the later timepoint
+    for link_in in np.unique(links_in):
+        subset = np.array(options[links_in==link_in, :])
+
+        rows = np.split(subset, subset.shape[0])
+
+        if (not complete_graph) or (np.sum(types[links_in==link_in]=='previous_position')>0):
+            rows.append(np.zeros(rows[0].shape))
+
+        if (np.sum(types[links_in==link_in]=='division')==1):
+            # encodes an disappearance + a division
+            rows.append(links_in==link_in)
+
+        independents.append(rows)
+
+    # combine all these vectors to form a set containing all possible flow configurations (but which are not all possible)
+    possibilities = []
+    for combination in itertools.product(*independents):
+
+        possibility = np.sum(list(combination), axis=0)
+
+        possibilities.append(np.squeeze(possibility))
+
+    return np.stack(possibilities, axis=0)
+
+
+def marginalization(energies, constraint_matrix, constraint_matrix_link, exact_match, exact_match_link, microstates=None):
+    """Performs marginalization"""
+
+    # construct all microstates (innefficenty) if none were given
+    if microstates is None:
+        possibilities = list(itertools.product([0, 1], repeat=len(energies)))
+        possibilities = np.stack(possibilities, axis=0)
+    else:
+        possibilities = microstates
+
+    # get energy per microstate
+    total_energy = np.matmul(possibilities, energies)
+
+    # check if the flows are allowed
+    total_flows = np.matmul(possibilities, constraint_matrix)
+    allowed = (np.sum(total_flows > 1, axis=-1) == 0)  & (np.sum((total_flows != 1) * exact_match, axis=-1) == 0)
+    total_probability = np.sum(10**(-total_energy[allowed]))
+
+    # check which flows are allowed if the link of interest is part of the solution
+    total_flows = np.matmul(possibilities, constraint_matrix_link)
+    allowed = (np.sum(total_flows > 1, axis=-1) == 0) & (np.sum((total_flows != 1) * exact_match_link, axis=-1) == 0)
+    total_probability_link = np.sum(10**(-total_energy[allowed]))
+
+    #marginalize
+    return total_probability_link/total_probability
+
+
+def local_marginalization(position1, position2, experiment, steps=3, verbose = False, complete_graph=False, scale=1):
+    """Constructs subgraph, retrieves flows and energies and performs marginalization"""
+
+    if position1.time_point_number() > position2.time_point_number():
+        position1, position2 = position2, position1
+
+    # get local and temporal environment
+    current_pos, previous_pos, local_links = find_local_set(position1, position2, experiment.links, steps=steps)
+
+    # check if subgraph size is not too big for typical memory. Makes an educated guess about number (log2) of microstates that will need to be constructed.
+    # If subset is to big lower the number of steps.
+    while (np.log2(len(local_links)/len(current_pos)+1) * len(current_pos) + len(previous_pos)) > 16:
+
+        if steps == 0:
+            if verbose:
+             print('do minimal marginalization instead')
+            return minimal_marginalization(position1, position2, experiment)
+
+        steps = steps-1
+        if verbose:
+         print('environment is too big')
+        current_pos, previous_pos, local_links = find_local_set(position1, position2, experiment.links, steps=steps)
+
+    # which cells are fully connected?
+    complete_in, complete_out = check_completeness(current_pos.union(previous_pos), experiment.links)
+
+    # set up equation
+    links_out, links_in, flow, energies, key_dict, types = get_flows_and_energies(previous_pos, current_pos, local_links, experiment, complete_graph=complete_graph)
+
+    # scale the energies
+    energies = energies*scale
+
+    # build constraint matrix
+    constraint_matrix, exact_match = build_constraint_matrix(previous_pos, current_pos, links_out, links_in, flow,
+                                                             key_dict, complete_in, complete_out, complete_graph=complete_graph)
+
+    # further constrain on existence of the link
+    constraint_matrix_link = np.array(constraint_matrix)
+    exact_match_link = np.array(exact_match)
+
+    link_id = np.where((links_in == key_dict[position2]) & (links_out == key_dict[position1]))
+    constraint_matrix_link[link_id, 0] = 1
+    exact_match_link[0] = True
+
+    # perform marginalization
+    return marginalization(energies, constraint_matrix, constraint_matrix_link, exact_match, exact_match_link,
+                           microstates=construct_microstates(links_out, links_in, types, complete_graph=complete_graph ))
+
+
+def energy_to_prob(energy):
+    return 10**-energy/(1+10**-energy)
+
+
+def prob_to_energy(prob):
+    epsilon = 10**-10
+    return np.log10((1-prob+epsilon)/(prob+epsilon))
+
+def combine_events(energies):
+    """combines mutually exclusive events"""
+    probs = energy_to_prob(np.array(energies))
+    prob = 1-np.product(1-probs)
+
+    return prob_to_energy(prob)
---
+++ organoid_tracker_marginalization.py
@@ -0,0 +1,113 @@
+"""Predictions particle positions using an already-trained convolutional neural network."""
+
+from organoid_tracker.core.position import Position
+from organoid_tracker.config import ConfigFile
+from organoid_tracker.core.resolution import ImageResolution
+from organoid_tracker.imaging import io
+from organoid_tracker.image_loading import general_image_loader
+
+from organoid_tracker.linking import cell_division_finder
+from organoid_tracker.linking_analysis import cell_error_finder
+from organoid_tracker.local_marginalization.local_marginalization_functions import local_marginalization, \
+    minimal_marginalization
+
+print("Hi! Configuration file is stored at " + ConfigFile.FILE_NAME)
+config = ConfigFile("marginalisation")
+_images_folder = config.get_or_prompt("images_container", "If you have a folder of image files, please paste the folder"
+                                      " path here. Else, if you have a LIF file, please paste the path to that file"
+                                      " here.", store_in_defaults=True)
+_images_format = config.get_or_prompt("images_pattern", "What are the image file names? (Use {time:03} for three digits"
+                                      " representing the time point, use {channel} for the channel)",
+                                      store_in_defaults=True)
+
+_experiment_file = config.get_or_default("solution_links_file",
+                                            "cleanAutomatic links.aut",
+                                            comment="What are the detected positions for those images?")
+_all_links_file = config.get_or_default("all_links_file",
+                                            "all_linksAutomatic links.aut",
+                                            comment="What are the detected positions for those images?")
+
+_min_time_point = int(config.get_or_default("min_time_point", str(1), store_in_defaults=True))
+_max_time_point = int(config.get_or_default("max_time_point", str(9999), store_in_defaults=True))
+
+_steps = int(config.get_or_default("size subset (steps away from link of interest)", str(3)))
+_temperature = float(config.get_or_default("temperature (to account for shared information)", str(1.5)))
+_filter_cut_off = float(config.get_or_default("maximum error rate", str(0.01)))
+
+# Load experiments
+experiment = io.load_data_file(_experiment_file, _min_time_point, _max_time_point)
+general_image_loader.load_images(experiment, _images_folder, _images_format,
+                                 min_time_point=_min_time_point, max_time_point=_max_time_point)
+
+experiment_all_links = io.load_data_file(_all_links_file, _min_time_point, _max_time_point)
+general_image_loader.load_images(experiment_all_links, _images_folder, _images_format,
+                                 min_time_point=_min_time_point, max_time_point=_max_time_point)
+
+_output_file = config.get_or_default("marginalized_output_file", "Marginalized positions.aut", comment="Output file for the positions, can be viewed using the visualizer program.")
+_filtered_output_file = config.get_or_default("filtered_output_file", "Filtered positions.aut", comment="Output file for the positions, can be viewed using the visualizer program.")
+
+config.save_and_exit_if_changed()
+# END OF PARAMETERS
+
+number_of_links = len(list(experiment.links.find_all_links()))
+index = 1
+
+# Marginalize
+for (position1, position2), link_penalty in experiment.link_data.find_all_links_with_data("link_penalty"):
+    print(f"Marginalizing {index}/{number_of_links}")
+
+    # if links are extremely (un)likely we do not have to perform marginalization over a large subgraph.
+    if abs(link_penalty) < 4.:
+        marginalized_probability = local_marginalization(position1, position2, experiment_all_links, complete_graph=True, steps=_steps, scale=1/_temperature)
+    else:
+        marginalized_probability = minimal_marginalization(position1, position2, experiment_all_links,  scale=1/_temperature)
+
+    experiment.link_data.set_link_data(position1, position2, data_name="marginal_probability",
+                                       value=marginalized_probability)
+
+    index = index +1
+
+# Assign errors based on marginalized scores
+print("Checking results for common errors...")
+warning_count, no_links_count = cell_error_finder.find_errors_in_experiment(experiment, marginalization=True)
+
+print("Saving files...")
+io.save_data_to_json(experiment, _output_file)
+
+# Filter data
+experiment_filtered = experiment.copy_selected(images = True, positions = True, position_data = True,
+                      links = True, link_data = True, global_data = True)
+
+mothers = cell_division_finder.find_mothers(experiment.links)
+count = 0
+
+for (position1, position2), marginal_probability in experiment.link_data.find_all_links_with_data("marginal_probability"):
+
+    if (marginal_probability is not None):
+        # remove uncertain links
+        if marginal_probability < (1-_filter_cut_off):
+            count = count + 1
+            experiment_filtered.links.remove_link(position1, position2)
+
+            # if the removed link is from a mother also remove the other outgoing links to avoid confusion about of it is dividing or not
+            if position1 in mothers:
+                for next_position in experiment.links.find_futures(position1):
+                    experiment_filtered.links.remove_link(position1, next_position)
+
+    # if the division probability is high, but no division is assigned, we add one to avoid misinterpretation
+    division_probability = experiment.position_data.get_position_data(position1, 'division_probability')
+
+    if (division_probability > 0.99) and (position2 not in mothers) and (position1 not in mothers):
+        add_position = Position(x=position2.x +1,
+                                    y=position2.y +1,
+                                    z=position2.z,
+                                    time_point=position2.time_point())
+
+        experiment_filtered.positions.add(add_position)
+        experiment_filtered.links.add_link(position1, add_position)
+
+print('removed uncertain links:')
+print(count)
+print("Saving files...")
+io.save_data_to_json(experiment_filtered, _filtered_output_file)
+
