7e6cb0bf41d05fa0b7d71c6fb6b8aeff04309b3f
Rutger Kok <rutger.kok@live.com>
2023-07-24 15:38:48 +0200

    Restore complex image saving functionality
    Apparently, this was lost during the merge. No idea why.

--- organoid_tracker_plugins/plugin_files_list_importer_exporter.py
+++ organoid_tracker_plugins/plugin_files_list_importer_exporter.py
@@ -10,8 +10,7 @@ from typing import List
 
 from organoid_tracker.core.experiment import Experiment
 from organoid_tracker.image_loading import general_image_loader
-from organoid_tracker.imaging import io
-
+from organoid_tracker.imaging import io, list_io
 
 FILES_LIST_EXTENSION = ".autlist"
 
@@ -27,46 +26,7 @@ def get_menu_items(window: Window) -> Dict[str, Any]:
 
 def _restore_open_files_list(open_files_list_file: str) -> List[Experiment]:
     """Returns a list of experiments, extracted from the given file."""
-    start_dir = os.path.dirname(open_files_list_file)
-    with open(open_files_list_file, "r") as handle:
-        experiments_json = json.load(handle)
-
-    experiments = list()
-    for experiment_json in experiments_json:
-        experiment = Experiment()
-        min_time_point = 0
-        max_time_point = 100000000
-
-        if "min_time_point" in experiment_json:
-            min_time_point = int(experiment_json["min_time_point"])
-        if "max_time_point" in experiment_json:
-            max_time_point = int(experiment_json["max_time_point"])
-
-        loaded_anything = False
-        if "experiment_file" in experiment_json:
-            experiment_file = experiment_json["experiment_file"]
-            if not os.path.isabs(experiment_file):
-                experiment_file = os.path.join(start_dir, experiment_file)
-            if not os.path.exists(experiment_file):
-                raise ValueError("File \"" + experiment_file + "\" does not exist.")
-            io.load_data_file(experiment_file, experiment=experiment,
-                              min_time_point=min_time_point, max_time_point=max_time_point)
-            loaded_anything = True
-
-        if "images_container" in experiment_json and "images_pattern" in experiment_json:
-            images_container = experiment_json["images_container"]
-            if not os.path.isabs(images_container):
-                images_container = os.path.join(start_dir, images_container)
-            images_pattern = experiment_json["images_pattern"]
-            general_image_loader.load_images(experiment, images_container, images_pattern,
-                                             min_time_point=min_time_point, max_time_point=max_time_point)
-            loaded_anything = True
-
-        if not loaded_anything:
-            raise ValueError("No experiment defined in " + json.dumps(experiment_json) + ".")
-
-        experiments.append(experiment)
-    return experiments
+    return list(list_io.load_experiment_list_file(open_files_list_file))
 
 
 def _save_open_file_list(tabs: Iterable[SingleGuiTab]) -> Optional[List[Dict[str, Any]]]:
@@ -97,10 +57,7 @@ def _save_open_file_list(tabs: Iterable[SingleGuiTab]) -> Optional[List[Dict[str
         if experiment.last_save_file is not None:
             experiment_json["experiment_file"] = experiment.last_save_file
 
-        images_container, images_pattern = experiment.images.image_loader().serialize_to_config()
-        if len(images_container) > 0 or len(images_pattern) > 0:
-            experiment_json["images_container"] = images_container
-            experiment_json["images_pattern"] = images_pattern
+        experiment_json.update(experiment.images.image_loader().serialize_to_dictionary())
 
         if experiment.first_time_point_number() is not None:
             experiment_json["min_time_point"] = experiment.first_time_point_number()
