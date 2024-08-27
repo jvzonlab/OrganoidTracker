ed0d3cde25b6aaf49fc316d42a437490104447a4
Rutger Kok <rutger.kok@live.com>
2024-08-27 14:58:42 +0200

    Allow position model created using older version of Keras 3 to load on newer version

--- _keras_environment.py
+++ _keras_environment.py
@@ -13,3 +13,11 @@ def activate():
 
         kernel = windll.kernel32
         kernel.SetConsoleMode(kernel.GetStdHandle(-11), 7)
+
+    # For loading models for a bit older Keras 3 versions
+    import keras
+    from keras.src import api_export
+    api_export.REGISTERED_NAMES_TO_OBJS["keras.models.functional.Functional"] = keras.src.models.functional.Functional
+    api_export.REGISTERED_NAMES_TO_OBJS["keras.ops.numpy.Concatenate"] = keras.src.ops.numpy.Concatenate
+    api_export.REGISTERED_NAMES_TO_OBJS["keras.ops.nn.Conv"] = keras.src.ops.nn.Conv
+    api_export.REGISTERED_NAMES_TO_OBJS["keras.backend.torch.optimizers.torch_adam.Adam"] = keras.src.optimizers.Adam
