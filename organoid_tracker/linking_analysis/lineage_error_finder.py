6e98547105551959334d36f00c803ff9515f0a83
Rutger Kok <rutger.kok@live.com>
2025-04-25 15:45:11 +0200

    Fix error settings in window.display_settings resurfacing
    Now everything is back again in experiment.warning_limits. Whenever these are changed, the GUI functions will recalculate the errors in the experiment.  Therefore, no additional filtering is necessary by linking_markers.find_errored_positions.

--- organoid_tracker/linking_analysis/lineage_error_finder.py
+++ organoid_tracker/linking_analysis/lineage_error_finder.py
@@ -119,10 +119,7 @@ def get_problematic_lineages(experiment: Experiment, crumbs: AbstractSet[Positio
                              excluded_errors=None) -> List[LineageWithErrors]:
     """Gets a list of all lineages with warnings in the experiment. The provided "crumbs" are placed in the right
     lineages, so that you can see to what lineages those cells belong."""
-    positions_with_errors = linking_markers.find_errored_positions(experiment.position_data,
-                                                                   min_time_point=min_time_point,
-                                                                   max_time_point=max_time_point,
-                                                                   excluded_errors=excluded_errors)
+    positions_with_errors = linking_markers.find_errored_positions(experiment.position_data)
     track_to_errors = _group_by_track(experiment.links, positions_with_errors)
     track_to_crumbs = _group_by_track(experiment.links, crumbs)
     tracks_to_focus_on = find_error_focus_tracks(experiment)
