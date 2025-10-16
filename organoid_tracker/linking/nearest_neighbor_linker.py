ab80d73f325aedf15a4105d2f4f4e07094cd50f5
Rutger Kok <rutger.kok@live.com>
2025-10-16 14:17:13 +0200

    Put the probability by distance code online

--- organoid_tracker/linking/nearest_neighbor_linker.py
+++ organoid_tracker/linking/nearest_neighbor_linker.py
@@ -1,10 +1,33 @@
 """Ultra-simple linker. Used as a starting point for more complex links."""
+import math
+from collections import defaultdict
+from typing import Union, Tuple
+
+import numpy
+import scipy
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
 from organoid_tracker.core import TimePoint
 from organoid_tracker.core.experiment import Experiment
 from organoid_tracker.core.images import Images
 from organoid_tracker.core.links import Links
 from organoid_tracker.core.position_collection import PositionCollection
 from organoid_tracker.linking.nearby_position_finder import find_close_positions
+
+
+class LogisticFit:
+    """Result of the fit of the sigmoid function to the link-is-correct probability per distance data."""
+
+    x0: float  # Midpoint, the distance at which the probability is 0.5
+    k: float  # Steepness of the curve
+
+    def __init__(self, popt: tuple[float, float]):
+        self.x0 = popt[0]
+        self.k = popt[1]
+
+    def __call__(self, x: Union[float, numpy.ndarray]) -> float:
+        return 1 - 1 / (1 + numpy.exp(-self.k * (x - self.x0)))
 
 
 def nearest_neighbor(experiment: Experiment, *, tolerance: float = 1.0, back: bool = True, forward: bool = True,
@@ -44,6 +67,77 @@ def nearest_neighbor(experiment: Experiment, *, tolerance: float = 1.0, back: bo
 
     print("Done creating nearest-neighbor links!")
     return links
+
+
+def nearest_neighbor_probabilities(experiment: Experiment, *, tolerance: float = 2.0, max_distance_um: float = 50,
+                                   distance_bin_size_um_log: float = 0.25) -> Tuple[Links, LogisticFit]:
+    """First does nearest-neighbor linking, and assumes all nearest links to be true, the rest false. Creates a
+    probability map of distance --> assumed correct. Then sets probabilities based on that.
+
+    Default tolerance is 2.0, which means that all links that are at most twice as long as the nearest link are also
+    taken into account as potential links.
+    """
+    links_nearest_neighbor = nearest_neighbor(experiment, tolerance=tolerance, back=True, forward=True,
+                                              max_distance_um=max_distance_um)
+    true_links_count_by_distance = defaultdict(lambda: 0)
+    false_links_count_by_distance = defaultdict(lambda: 0)
+
+    resolution = experiment.images.resolution()
+    for time_point in experiment.positions.time_points():
+        for position in experiment.positions.of_time_point(time_point):
+            futures = links_nearest_neighbor.find_futures(position)
+            if len(futures) == 0:
+                continue  # No links, skip
+            log_distances = numpy.log([position.distance_um(future, resolution) + 0.001 for future in futures])
+            log_distances.sort()
+
+            # First link is assumed true, the rest false
+            true_links_count_by_distance[int(log_distances[0] // distance_bin_size_um_log)] += 1
+            for log_distance in log_distances[1:]:
+                distance_bin = int(log_distance // distance_bin_size_um_log)
+                false_links_count_by_distance[distance_bin] += 1
+
+    if len(true_links_count_by_distance) == 0 or len(false_links_count_by_distance) == 0:
+        raise RuntimeError("No links were created, cannot create probability map.")
+
+    # Now create a probability map based on the counts
+    max_found_distance_key = max(max(true_links_count_by_distance.keys()), max(false_links_count_by_distance.keys()))
+    probabilties_by_distance = {}
+    for i in range(max_found_distance_key + 1):
+        total = true_links_count_by_distance.get(i, 0) + false_links_count_by_distance.get(i, 0)
+        if total == 0:
+            probabilties_by_distance[i] = 0.0
+        else:
+            probabilties_by_distance[i] = true_links_count_by_distance.get(i, 0) / total
+
+    # Add some extra bins with zero probability to help the fit
+    # At large distances cells should never link, and at tiny distances they should always link.
+    # But don't use exactly 0 or 1, as that doesn't work with the sigmoid fit.
+    max_index = max(probabilties_by_distance.keys())
+    min_index = min(probabilties_by_distance.keys())
+    for i in range(max_index + 1, max_index + 10):
+        probabilties_by_distance[i] = 0.001
+    for i in range(min_index - 10, min_index - 1):
+        probabilties_by_distance[i] = 0.999
+
+    # Now set the probabilities in the links
+    xdata = numpy.exp(numpy.array(list(probabilties_by_distance.keys()), dtype=float) * distance_bin_size_um_log)
+    ydata = numpy.array(list(probabilties_by_distance.values()), dtype=float)
+
+    def sigmoid(x, x0, k):
+        y = 1 - 1 / (1 + numpy.exp(-k * (x - x0)))
+        return y
+    p0 = [numpy.median(xdata), 1]  # This is a mandatory initial guess
+    popt, pcov = scipy.optimize.curve_fit(sigmoid, xdata, ydata, p0, method='lm')
+    fit_sigmoid = LogisticFit(popt)
+
+    for position_a, position_b in links_nearest_neighbor.find_all_links():
+        distance_um = position_a.distance_um(position_b, resolution)
+        probability = fit_sigmoid(distance_um)
+        likelihood = math.log10(probability) - math.log10(1 - probability)
+        links_nearest_neighbor.set_link_data(position_a, position_b, data_name="link_probability", value=probability)
+        links_nearest_neighbor.set_link_data(position_a, position_b, data_name="link_penalty", value=-likelihood)
+    return links_nearest_neighbor, fit_sigmoid
 
 
 def _add_nearest_edges(links: Links, positions: PositionCollection, images: Images, time_point_previous: TimePoint,
