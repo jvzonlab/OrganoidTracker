cbd5effcd73b52e9c4ed3d0b3ffc7bece1cc7e72
Rutger Kok <rutger.kok@live.com>
2025-11-14 18:13:50 +0100

    Rewrite position prediction to work on arbitrarily large images
    It now also rescales to the target resolution of the model. This should make the model work better out of the box.
    
    Reconstruction of the full predicted volume hasn't been added back yet.

--- organoid_tracker/neural_network/position_detection_cnn/peak_calling.py
+++ organoid_tracker/neural_network/position_detection_cnn/peak_calling.py
@@ -40,21 +40,3 @@ def reconstruct_volume(multi_im: ndarray, mid_layers_nb: int) -> Tuple[ndarray, 
             layer_index += 1
 
     return out_img, mid_layers_nb + 1
-
-
-
-def create_prediction_mask(radius_x_px_float: float, resolution: ImageResolution) -> ndarray:
-    """Creates a mask that is spherical in micrometers. If the resolution is not the same in the x, y and z directions,
-    this sphere will appear as a spheroid in the images.
-
-    For use with the footprint param in scikit-image peak_local_max."""
-    radius_x_px = int(math.ceil(radius_x_px_float))
-    radius_y_px = int(math.ceil(radius_x_px_float) * resolution.pixel_size_x_um / resolution.pixel_size_y_um)
-    radius_z_px = int(math.ceil(radius_x_px_float * resolution.pixel_size_x_um / resolution.pixel_size_z_um))
-    mask = Mask(bounding_box.ONE.expanded(radius_x_px, radius_y_px, radius_z_px))
-
-    # Evaluate the spheroid function to draw it
-    mask.add_from_function(lambda x, y, z:
-                           x ** 2 / radius_x_px ** 2 + y ** 2 / radius_y_px ** 2 + z ** 2 / radius_z_px ** 2 <= 1)
-
-    return mask.get_mask_array()
