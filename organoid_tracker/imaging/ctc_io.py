fd3703116055c4f682e668c1cd6e165399b00cdc
maxbetjes <69514180+maxbetjes@users.noreply.github.com>
2026-08-06 12:55:40 +0200

    Open CTC files with arbitrary number of leading zeros.

--- organoid_tracker/imaging/ctc_io.py
+++ organoid_tracker/imaging/ctc_io.py
@@ -73,17 +73,28 @@ def load_data_file(file_name: str, min_time_point: int = 0, max_time_point: int 
     links_to_mother_by_time_point = _read_lineage_file(file_name)
 
     file_prefix = file_name[:-4]
+
+    # check number of leading zeros
+    directory = os.path.dirname(file_name)
+    files = os.listdir(directory)
+
+    for file in files:
+        if file.endswith('.tif'):
+            continue
+    number_length = len(directory + '/' + file[:-4]) - len(file_prefix)
+
     time_point_number = 0
     positions_of_previous_time_point = dict()
 
-    while os.path.exists(f"{file_prefix}{time_point_number:03}.tif"):
+    while os.path.exists(f"{file_prefix}{time_point_number:0{number_length}}.tif"):
+
         if time_point_number < min_time_point:
             continue
         if time_point_number > max_time_point:
             break
 
         print(f"Working on time point {time_point_number}...")
-        image = tifffile.imread(f"{file_prefix}{time_point_number:03}.tif")
+        image = tifffile.imread(f"{file_prefix}{time_point_number:0{number_length}}.tif")
         regionprops = skimage.measure.regionprops(image)
         positions_of_time_point = dict()
 
